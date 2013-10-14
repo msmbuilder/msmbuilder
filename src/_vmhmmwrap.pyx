32d7f91573f773a4867331cb6914c6d8ee516824
Robert McGibbon <rmcgibbo@gmail.com>
2013-10-14 15:23:11 -0700

    typo

--- src/_vmhmmwrap.pyx
+++ src/_vmhmmwrap.pyx
@@ -19,3 +19,23 @@ def _fitinvkappa(np.ndarray[np.double_t, ndim=2, mode="c"] posteriors not None,
     fitinvkappa(n_samples, n_features, n_components, &posteriors[0, 0],
                 &obs[0,0], &means[0, 0], &out[0, 0])
     return 1;
+
+
+def _compute_log_likelihood_1(obs, means, kappas):
+    from scipy.stats.distributions import vonmises
+    n_components = kappas.shape[0]
+    value = np.array([np.sum(vonmises.logpdf(obs, kappas[i], means[i]), axis=1) for i in range(n_components)]).T
+    return value
+
+def _compute_log_likelihood_2(obs, means, kappas):
+    return obs
+
+
+obs = np.random.rand(10,3)
+means = np.random.rand(2,3)
+kappas = np.random.rand(2,3)
+A = _compute_log_likelihood_1(obs, means, kappas)
+B = _compute_log_likelihood_2(obs, means, kappas)
+
+np.testing.assert_array_almost_equal(A, B)
+exit(1)
