0290c741643b3ce38306e4459a74c8597c6bea8d
Robert McGibbon <rmcgibbo@gmail.com>
2014-01-17 18:50:52 -0800

    Pull structures working better

---
+++ Mixtape/utils.py
@@ -0,0 +1,31 @@
+import json
+import numpy as np
+import mdtraj as md
+
+
+def iterobjects(fn):
+    for line in open(fn, 'r'):
+        if line.startswith('#'):
+            continue
+        try:
+            yield json.loads(line)
+        except ValueError:
+            pass
+
+
+def load_timeseries(filenames, atom_indices, topology):
+    X = []
+    i = []
+    f = []
+    for file in filenames:
+        t = md.load(file)
+        t.superpose(topology, atom_indices=atom_indices)
+        diff2 = (t.xyz[:, atom_indices] - topology.xyz[0, atom_indices])**2
+        x = np.sqrt(np.sum(diff2, axis=2))
+
+        X.append(x)
+        i.append(np.arange(len(x)))
+        f.extend([file]*len(x))
+
+    return np.concatenate(X), np.concatenate(i), np.array(f)
+
