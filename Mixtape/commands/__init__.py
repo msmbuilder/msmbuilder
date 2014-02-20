3f5ff14f5a81c380eb7d91f2becd1a2dc06d2ca1
kyleabeauchamp <kyleabeauchamp@gmail.com>
2014-02-19 19:05:15 -0500

    Update __init__.py

--- Mixtape/commands/__init__.py
+++ Mixtape/commands/__init__.py
@@ -2,4 +2,4 @@ from .fitem import FitEM
 from .atomindices import AtomIndices
 from .inspect_output import Inspect
 
-__all__ = ['FitEM', 'AtomIndices', 'Inspect]
+__all__ = ['FitEM', 'AtomIndices', 'Inspect']
