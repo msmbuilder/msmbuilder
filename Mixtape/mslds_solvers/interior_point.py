c347ca961982bc737328828053e156df1d014017
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2014-04-09 11:11:03 -0700

    It becomes clear that this will be no small endeavor

--- Mixtape/mslds_solvers/interior_point.py
+++ Mixtape/mslds_solvers/interior_point.py
@@ -51,16 +51,24 @@ class InteriorPointSolver(object):
 
         phi(X) = -log(-g_i(X))
 
-    And solve the central path solution x*(t) of the problem below
+    We then iteratively solve the central path solution x*(t) of the
+    problem below
 
         min t f(X) + phi(X)
         subject to
             Ax = b
+
+    through constrained Newton optimization of the KKT conditions
     """
-    def __init__(self, f, gs, grad_gs, hessian_gs, A, b):
+    def __init__(self, f, grad_f, hessian_f,
+            gs, grad_gs, hessian_gs, A, b):
         """
         f: function
             Objective to maximize
+        grad_f: function
+            Gradient of objective
+        hessian_f: function
+            Hessian of objective
         gs: list
             list of constraint functions g_i
         grad_gs: list
@@ -77,37 +85,93 @@ class InteriorPointSolver(object):
 
         self.dim = dim
         self.f = f
+        self.grad_f = grad_f
+        self.hessian_f = hessian_f
         self.gs = gs
         self.grad_gs = grad_gs
         self.hessian_gs = hessian_gs
         self.A = A
         self.b = b
 
-    def barrier_method(self, eps, mu):
-        """
-        eps: float
-            errror tolerance
-        mu: float
-            mu > 0 provides the scaling per outer step of the barrier
-            method.
-        """
-        X = None
-        while True:
-            X = self.newton_method_with_equality(t)
-            if 1./t < eps:
-                break
-            t = mu * t
-        return X
+def barrier_method(eps, mu, f, grad_f, hessian_f,
+        phi, grad_phi, hessian_phi, A, b):
+    """
+    eps: float
+        errror tolerance
+    mu: float
+        mu > 0 provides the scaling per outer step of the barrier
+        method.
+    f: function
+        Optimization objective
+    grad_f: function
+        Gradient of objective
+    hessian_f: function
+        Hessian of objective
+    phi: function
+        Barrier method
+    grad_phi: function
+        Gradient of Barrier
+    hessian_phi: function
+        Hessian of Barrier
+    A: numpy.ndarray
+        Constraint matrix
+    b: numpy.ndarray
+        Constraint Vector
+    """
+    X = None
+    while True:
+        def g(X):
+            return t*f(X) +
+        X = newton_method_with_equality(X, eps, t)
+        if 1./t < eps:
+            break
+        t = mu * t
+    return X
 
-    def newton_method_with_equality(self, X, t, eps):
-        while True:
-            delta_xnt = self.compute_newton_step()
-            lambda_x = self.compute_newton_decrement()
-            if lambda_x**2/2. < eps:
-                break
-            t = self.backtracking_line_search(X, delta_xnt, lambda_x)
-            X = X + t * delta_xnt
-        return X
+def feasible_start_newton_method(X, eps, g, grad_g, hessian_g, A, b):
+    """
+    X: numpy.ndarray
+        feasible starting point
+    eps: float
+        error tolerance, eps > 0
+    g: function
+        function to optimize
+    grad_g: function
+        Gradient of objective
+    hessian_g: function
+        Hessian of objective
+    A: numpy.ndarray
+        constraint matrix
+    b: numpy.ndarray
+        Tr(A X) = b
+    """
+    while True:
+        delta_xnt = compute_newton_step()
+        lambda_x = compute_newton_decrement()
+        if lambda_x**2/2. < eps:
+            break
+        t = backtracking_line_search(X, delta_xnt, lambda_x)
+        X = X + t * delta_xnt
+    return X
+
+def infeasible_start_newton_method(eps, g, grad_g, hessian_g, A, b):
+    """
+    X: numpy.ndarray
+        feasible starting point
+    eps: float
+        error tolerance, eps > 0
+    g: function
+        function to optimize
+    grad_g: function
+        Gradient of objective
+    hessian_g: function
+        Hessian of objective
+    A: numpy.ndarray
+        constraint matrix
+    b: numpy.ndarray
+        Tr(A X) = b
+    """
+    pass
 
 def f():
     pass
