985f1d94bbe7cb80d6835b7b42d82d01f4fd4d8e
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2014-04-13 20:34:12 -0700

    Reformulated objective function for feasibility slightly

--- Mixtape/mslds_solvers/test_bounded_trace_sdp_solver.py
+++ Mixtape/mslds_solvers/test_bounded_trace_sdp_solver.py
@@ -29,10 +29,87 @@ def gradf(x):
         G[i,i] += -2.*x[i,i]
     return G
 
+def penalty(X, m, n, M, As, bs, Cs, ds):
+    """
+    Computes
+    f(X) = -(1/M) log(sum_{i=1}^m exp(M*(Tr(Ai,X) - bi))
+                    + sum_{j=1}^n exp(M*(Tr(Cj,X) - dj)^2))
+
+    Parameters
+    __________
+
+    m: int
+        Number of inequaltity constraints
+    n: int
+        Number of equality constraints
+    M: float
+        Rescaling Factor
+    """
+    s = 0.
+    r = 0.
+    retval = 0.
+    for i in range(m):
+        Ai = As[i]
+        bi = bs[i]
+        s += np.exp(M*(np.trace(np.dot(Ai,X)) - bi))
+    for j in range(n):
+        Cj = Cs[j]
+        dj = ds[j]
+        r += np.exp(M*(np.trace(np.dot(Cj,X)) - dj)**2)
+    if m + n > 0:
+        retval += -(1.0/M) * np.log(s + r)
+    return retval
+
+def grad_penalty(X, m, n, M, As, bs, Cs, ds, dim):
+    """
+    Computes grad f(X) = -(1/M) * c' / c where
+      c' = (sum_{i=1}^m exp(M*(Tr(Ai, X) - bi)) * (M * Ai.T)
+            + sum_{j=1}^n exp(M(Tr(Cj,X) - dj)**2)
+                            * (2M(Tr(Cj,X) - dj)) * Cj.T)
+      c  = (sum_{i=1}^m exp(M*(Tr(Ai,X) - bi))
+            + sum_{i=1}^n exp(M(Tr(Cj,X) - dj)**2))
+    """
+    retval = 0.
+    num = 0.
+    denom = 0.
+    for i in range(m):
+        Ai = As[i]
+        bi = bs[i]
+        if dim >= 2:
+            num += np.exp(M*(np.trace(np.dot(Ai,X)) - bi))*(M*Ai.T)
+            denom += np.exp(M*(np.trace(np.dot(Ai,X)) - bi))
+        else:
+            num += np.exp(M*(Ai*X - bi))*(M*Ai.T)
+            denom += np.exp(M*(Ai*X - bi))
+    for j in range(n):
+        Cj = Cs[j]
+        dj = ds[j]
+        if dim >= 2:
+            num += (np.exp(M*(np.trace(np.dot(Cj,X)) - dj)**2)*
+                    (2*M*(np.trace(np.dot(Cj,X)) - dj))*
+                    Cj.T)
+            denom += np.exp(M*(np.trace(np.dot(Cj,X)) - dj)**2)
+        else:
+            num += np.exp(M*(Cj*x - dj)**2)*(2*M*(Cj*x - dj))*Cj.T
+            denom += np.exp(M*(Cj*x - dj)**2)
+    if m + n > 0:
+        retval += -(1.0/M) * num/denom
+    return retval
+
 def test1():
     """
-     Do a simple test of the Bounded Trace Solver on function
-     f(x)  = -\sum_k x_kk^2 defined above.
+    Do a simple test of the Bounded Trace Solver on function
+    f(x)  = -\sum_k x_kk^2 defined above.
+
+    Now do a dummy optimization problem. The
+    problem we consider is
+
+        max - sum_k x_k^2
+        subject to
+            sum_k x_k = 1
+
+    The optimal solution is -1/n, where
+    n is the dimension.
     """
     # dimension of square matrix X
     dims = [1,2,4,8,16,32,64,128]
@@ -41,12 +118,6 @@ def test1():
         # Note that H(-f) = 2 I (H is the hessian of f)
         Cf = 2.
         N_iter = 2 * dim
-        # Now do a dummy optimization problem. The
-        # problem we consider is
-        # max - \sum_k x_k^2
-        # such that \sum_k x_k = 1
-        # The optimal solution is -1/n, where
-        # n is the dimension.
         b = BoundedTraceSDPHazanSolver()
         X = b.solve(f, gradf, dim, N_iter, Cf=Cf)
         #print("\tX:")
@@ -60,103 +131,39 @@ def test1():
         assert np.abs(fX - (-1./dim)) < 1./N_iter
         print("\tError Tolerance Acceptable")
 
+
 def test2():
     """
     Check that the bounded trace implementation can handle equality type
     constraints.
+
+    With As and bs as below, we solve the problem
+
+        max penalty(X)
+        subject to
+          x_11 + 2 x_22 == 1.5
+          Tr(X) = x_11 + x_22 == 1.
+
+    We should find penalty(X) >= -eps, and that the above constraints have
+    a solution
     """
     m = 0
     n = 1
     dim = 2
     N_iter = 50
     eps = 1./N_iter
-    if m > 0:
-        M = np.max((np.log(m), 1.))/eps
+    M = 1.
+    if m + n > 0:
+        M = 0.
+        if m > 0:
+            M += np.max((np.log(m), 1.))/eps
+        if n > 0:
+            M += np.max((np.log(n), 1.))/(eps**2)
         print("M", M)
-    if n > 0:
-        N = np.max((np.log(n), 1.))/(eps**2)
-        print("N", N)
     def f(X):
-        """
-        f(X) = -(1/M) log(sum_{i=1}^m exp(M*(Tr(Ai,X) - bi))) +
-               -(1/N) log( sum_{j=1}^n exp(-M*(Tr(Cj,X) - dj)^ 2))
-        """
-        s = 0.
-        r = 0.
-        retval = 0.
-        for i in range(m):
-            Ai = As[i]
-            bi = bs[i]
-            s += np.exp(M*(np.trace(np.dot(Ai,X)) - bi))
-        if m > 0:
-            retval += -(1.0/M) * np.log(s)
-        for j in range(n):
-            Cj = Cs[j]
-            dj = ds[j]
-            #r += np.exp(-M*(np.trace(np.dot(Cj,X) - dj)**2))
-            #print("Cj\n", Cj)
-            #print("X\n", X)
-            #print("(np.trace(np.dot(Cj,X) - dj)**2): ",
-            #        (np.trace(np.dot(Cj,X)) - dj)**2)
-            r += np.exp(N*(np.trace(np.dot(Cj,X)) - dj)**2)
-        if n > 0:
-            #retval += -(1.0/N) * np.log(r)
-            retval += -(1.0/N) * np.log(r)
-        return retval
+        return penalty(X, m, n, M, As, bs, Cs, ds)
     def gradf(X):
-        """
-        Computes grad f(X) = -(1/M) * f' / f where
-          f' = (sum_{i=1}^m exp(M*(Tr(Ai, X) - bi)) * (M * Ai.T)
-    + sum_{j=1}^n exp(-M(Tr(Cj,X) - dj)**2) * (-2M(Tr(Cj,X) - dj)) * Cj.T
-          f  = (sum_{i=1}^m exp(M*(Tr(Ai,X) - bi))
-            + sum_{i=1}^n exp(-M(Tr(Cj,X) - dj)**2))
-        """
-        retval = 0.
-        num1 = 0.
-        denom1 = 0.
-        for i in range(m):
-            Ai = As[i]
-            bi = bs[i]
-            if dim >= 2:
-                num1 += np.exp(M*(np.trace(np.dot(Ai,X)) - bi))*(M*Ai.T)
-                denom1 += np.exp(M*(np.trace(np.dot(Ai,X)) - bi))
-            else:
-                num1 += np.exp(M*(Ai*X - bi))*(M*Ai.T)
-                denom1 += np.exp(M*(Ai*X - bi))
-        if m > 0:
-            retval += (-1.0/M) * num1/denom1
-        num2 = 0.
-        denom2 = 0.
-        for j in range(n):
-            Cj = Cs[j]
-            dj = ds[j]
-            if dim >= 2:
-                #num2 += (np.exp(-N*(np.trace(np.dot(Cj,X)) - dj)**2)*
-                #        (-2*N*(np.trace(np.dot(Cj,X)) - dj))*
-                #        Cj.T)
-                #denom2 += np.exp(-N*(np.trace(np.dot(Cj,X)) - dj)**2)
-                num2 += (np.exp(N*(np.trace(np.dot(Cj,X)) - dj)**2)*
-                        (2*N*(np.trace(np.dot(Cj,X)) - dj))*
-                        Cj.T)
-                denom2 += np.exp(N*(np.trace(np.dot(Cj,X)) - dj)**2)
-            else:
-                #num2 += np.exp(-N*(cj*x - dj)**2)*(-2*m*(cj*x - dj))*cj.t
-                #denom2 += np.exp(-N*(cj*x - dj)**2)
-                num2 += np.exp(N*(Cj*x - dj)**2)*(2*N*(Cj*x - dj))*Cj.T
-                denom2 += np.exp(N*(Cj*x - dj)**2)
-        if n > 0:
-            #retval += (-1.0/N) * num2/denom2
-            retval += -(1.0/N) * num2/denom2
-        return retval
-    # With As and bs as below, we specify
-    # x_11 + 2 x_22 <= 1.5
-    # -x_11 - 2 x_22 <= 1.5
-    # That is, we encode equality x_11 + 2x_22 = 1.5 as two
-    #As = [np.array([[ 1.,  0.],
-    #                [ 0.,  2.]]),
-    #      np.array([[-1., -0.],
-    #                [-0., -2.]])]
-    #bs = [1.5, 1.5]
+        return grad_penalty(X, m, n, M, As, bs, Cs, ds, dim)
     As = []
     bs = []
     Cs = [np.array([[ 1.,  0.],
@@ -170,8 +177,45 @@ def test2():
     print("f(X) = %f" % (fX))
     FAIL = (fX < -eps)
     print("FAIL: " + str(FAIL))
-    #pdb.set_trace()
-    pass
+
+def test3():
+    """
+    Check that the bounded trace implementation can handle equality and
+    inequality type constraings
+
+    With As and bs as below, we solve the problem
+
+        max penalty(X)
+        subject to
+            x_11 + 2 x_22 <= 1
+            x_11 + 2 x_22 + 2 x_33 == 5/3
+            Tr(X) = x_11 + x_22 + x_33 == 1
+    """
+    m = 1
+    n = 1
+    dim = 3
+    N_iter = 50
+    eps = 1./N_iter
+    M = 1.
+    if m + n > 0:
+        M = 0.
+        if m > 0:
+            M += np.max((np.log(m), 1.))/eps
+        if n > 0:
+            M += np.max((np.log(n), 1.))/(eps**2)
+        print("M", M)
+    def f(X):
+        return penalty(X, m, n, M, As, bs, Cs, ds)
+    def gradf(X):
+        return grad_penalty(X, m, n, M, As, bs, Cs, ds, dim)
+    As = [np.array([[ 1., 0., 0.],
+                    [ 0., 2., 0.]
+                    [ 0., 0., 0.]])]
+    bs = [1.]
+    Cs = [np.array([[ 1.,  0., 0.],
+                    [ 0.,  2., 0.],
+                    [ 0.,  0., 2.]])]
+    ds = [5./3]
 
 if __name__ == "__main__":
     #test1()
