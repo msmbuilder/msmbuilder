08225b7b879a08e3397a250f364beb69aef79e9e
Bharath Ramsundar <bharath.ramsundar@gmail.com>
2014-05-07 13:45:43 -0700

    Removed old Q-test

--- Mixtape/mslds_solvers/test_general_sdp_solver.py
+++ Mixtape/mslds_solvers/test_general_sdp_solver.py
@@ -69,7 +69,7 @@ def testSchurComplement():
           --------------
     X is PSD
     """
-    dim = 2
+    dim = 1
     cdim = 3 * dim
     g = GeneralSDPHazanSolver()
     As, bs, Cs, ds, = [], [], [], []
@@ -78,6 +78,7 @@ def testSchurComplement():
     # Generate initial data
     D = np.eye(dim)
     Dinv = np.linalg.inv(D)
+    B = np.eye(dim)
     A = 0.5 * np.eye(dim)
     """ We neeed to enforce constant equalities in X.
       -------------
@@ -121,33 +122,22 @@ C =  | I        _    0 |
     Gs.append(linear_regions)
     gradGs.append(grad_linear_regions)
 
-    # - log det R
+    # - log det R + Tr(RB)
     def h(X):
         R = get_entries(X, R_cds)
-        return -np.log(np.linalg.det(R))
-    # grad - log det R = -R^{-1} (see Boyd and Vandenberge, A4.1)
-    # Note -R^{-1} = -Q
+        return -np.log(np.linalg.det(R)) + np.trace(np.dot(R, B))
+    # grad - log det R = -R^{-1} = -Q (see Boyd and Vandenberge, A4.1)
+    # grad tr(RB) = B^T
     def gradh(X):
         grad = np.zeros(np.shape(X))
         R = get_entries(X, R_cds)
-        # Look into avoiding this computation if possible
-        eigs_R = np.linalg.eigh(R)[0]
-        max_eig_R = np.amax(eigs_R)
-        max_eig_Q = 1./max_eig_R
         Q = np.linalg.inv(R)
-        # Scale Q down to unit spectral norm
-        Q = (1/max_eig_Q) * Q
-        # Scale Q to have spectral norm the same as Q
-        #Qinv *= max_eig_Q
-        gradR = Q
+        gradR = -Q + B.T
         set_entries(grad, R_cds, gradR)
         set_entries(grad, block_1_R_cds, gradR)
-        return -grad
+        return grad
 
-    #D_upper = np.trace(D)
-    #D_inv_upper = np.trace(Dinv)
-    #R = (D_upper + D_inv_upper + D_inv_upper)
-    R = 5 # Just going to set this high...
+    R = 5
     L = -20
     U = 20
     eps = 3e-2
@@ -168,169 +158,6 @@ C =  | I        _    0 |
     print "X_upper\n", X_upper
     if X_upper != None:
         print "h(X_upper)\n", h(X_upper)
-
-
-def testQ():
-    """
-    Specifies a simple version of the convex program required for
-    Q optimization
-
-    Given A, F = B^{.5},
-
-    We want to solve for x = [s vec(Z) vec(Q)]. To do so, we
-    construct and solve the following optimization problem
-
-    max Tr Z + log det Q^{-1}
-
-          -----------------------
-         | Z   F                 |
-         |F.T  Q                 |
-         |        D-Q   A        |
-    X =  |        A.T D^{-1}     |
-         |                   Q   |
-         |                     Z |
-          -----------------------
-    X is PSD
-
-    If Q is dim by dim, then this matrix is
-    6 * dim by 6 * dim
-
-    TODO: Think of how to shrink this representation
-    Ideas:
-        1) Add specific zero penalty operations so
-           we don't need to do python for-loops. ====> Done
-        2) Extend Hazan's method to simultaneously maintain multiple
-           PSD matrices instead of just one.
-    """
-    dim = 1
-    cdim = 6 * dim
-    g = GeneralSDPHazanSolver()
-    As, bs, Cs, ds, = [], [], [], []
-    Fs, gradFs, Gs, gradGs = [], [], [], []
-
-    # Generate initial data
-    B = 1 * np.eye(dim)
-    D = np.eye(dim)
-    Dinv = np.linalg.inv(D)
-    F = scipy.linalg.sqrtm(B)
-    A = np.eye(dim)
-
-    """ We neeed to enforce constant equalities in X.
-      ------------------------
-     | _   F   0    0    0  0 |
-     |F.T  _   0    0    0  0 |
-     | 0   0  D-_   A    0  0 |
-C =  | 0   0  A.T D^{-1} 0  0 |
-     | 0   0   0    0    _  0 |
-     | 0   0   0    0    0  _ |
-      ------------------------
-    """
-    C = np.zeros((cdim, cdim))
-    F_1_cds = (0, dim, dim, 2*dim)
-    F_T_1_cds = (dim, 2*dim, 0, dim)
-
-    A_2_cds = (2*dim, 3*dim, 3*dim, 4*dim)
-    A_T_2_cds = (3*dim, 4*dim, 2*dim, 3*dim)
-
-    Dinv_2_cds = (3*dim, 4*dim, 3*dim, 4*dim)
-
-    Z_1_below =  np.zeros((4*dim, 2*dim))
-    Z_1_below_cds = (2*dim, 6*dim, 0, 2*dim)
-
-    Z_1_right =  np.zeros((2*dim, 4*dim))
-    Z_1_right_cds = (0, 2*dim, 2, 6*dim)
-
-    Z_2_below = np.zeros((2*dim, 2*dim))
-    Z_2_below_cds = (4*dim, 6*dim, 2*dim, 4*dim)
-
-    Z_2_right = np.zeros((2*dim, 2*dim))
-    Z_2_right_cds = (2*dim, 4*dim, 4*dim, 6*dim)
-
-    Z_3_below = np.zeros((dim, dim))
-    Z_3_below_cds = (5*dim, 6*dim, 4*dim, 5*dim)
-
-    Z_3_right = np.zeros((dim, dim))
-    Z_3_right_cds = (4*dim, 5*dim, 5*dim, 6*dim)
-
-    constraints = [(F_1_cds, F), (F_T_1_cds, F.T), (A_2_cds, A),
-        (A_T_2_cds, A.T), (Dinv_2_cds, Dinv),
-        (Z_1_below_cds, Z_1_below), (Z_1_right_cds, Z_1_right),
-        (Z_2_below_cds, Z_2_below), (Z_2_right_cds, Z_2_right),
-        (Z_3_below_cds, Z_3_below), (Z_3_right_cds, Z_3_right)]
-    def const_regions(X):
-        return many_batch_equals(X, constraints)
-    def grad_const_regions(X):
-        return grad_many_batch_equals(X, constraints)
-    Gs.append(const_regions)
-    gradGs.append(grad_const_regions)
-
-    """ We need to constraint linear inequalities
-          -----------------------
-         | Z                     |
-         |     Q                 |
-         |        D-Q            |
-    C =  |                       |
-         |                   Q   |
-         |                     Z |
-          -----------------------
-    """
-
-    Z_cds = (5*dim, 6*dim, 5*dim, 6*dim)
-    Q_cds = (4*dim, 5*dim, 4*dim, 5*dim)
-    block_1_Z_cds = (0, dim, 0, dim)
-    block_1_Q_cds = (dim, 2*dim, dim, 2*dim)
-    block_2_Q_cds = (2*dim, 3*dim, 2*dim, 3*dim)
-
-    linear_constraints = \
-            [( 1., Q_cds, np.zeros((dim, dim)), block_1_Q_cds),
-             (-1., Q_cds, D, block_2_Q_cds),
-             ( 1., Z_cds, np.zeros((dim, dim)), block_1_Z_cds)]
-
-    def linear_regions(X):
-        return many_batch_linear_equals(X, linear_constraints)
-    def grad_linear_regions(X):
-        return grad_many_batch_linear_equals(X, linear_constraints)
-    Gs.append(linear_regions)
-    gradGs.append(grad_linear_regions)
-
-    # Tr Z + log det Q^{-1}
-    def h(X):
-        Z = get_entries(X, Z_cds)
-        Q = get_entries(X, Q_cds)
-        return np.trace(Z) - np.log(np.linalg.det(Q))
-    # grad log det Q^{-1} = Q (see Boyd and Vandenberge, A4.1)
-    # grad h = I_Z + Q
-    def gradh(X):
-        grad = np.zeros(np.shape(X))
-        gradQ = get_entries(X, Q_cds)
-        gradZ = np.eye(dim)
-        set_entries(X, Z_cds, gradZ)
-        set_entries(X, block_1_Z_cds, gradZ)
-        set_entries(grad, Q_cds, gradQ)
-        set_entries(grad, block_1_Q_cds, gradQ)
-        set_entries(grad, block_2_Q_cds, gradQ)
-        return grad
-
-    N_iter = 100
-    eps = 1.e-2
-    D_upper = np.trace(D)
-    D_inv_upper = np.trace(Dinv)
-    Z_upper = np.trace(np.dot(F, np.dot(Dinv, F.T)))
-    R = (Z_upper + D_upper + D_upper + D_inv_upper + D_upper + Z_upper)
-    L = 0
-    U = 2 * Z_upper
-    print "R: ", R
-
-    X_init = np.zeros((cdim, cdim))
-    Q_init = 0.2 * np.eye(dim)
-    set_entries(X_init, Q_cds, Q_init)
-    set_entries(X_init, block_1_Q_cds, Q_init)
-    set_entries(X_init, block_2_Q_cds, D - Q_init)
-    import pdb
-    pdb.set_trace()
-    upper, lower, X_upper, X_lower, SUCCEED = g.solve(h, gradh, As, bs,
-                Cs, ds, Fs, gradFs, Gs, gradGs, eps, cdim, R, U, L,
-                N_iter, X_init=X_init)
 
 def testA():
     """
