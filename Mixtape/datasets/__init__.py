8fc197561cacb2a64c1d049c29949fc0fb83123e
Robert McGibbon <rmcgibbo@gmail.com>
2014-05-15 21:25:34 -0700

    Add doublewell example dataset

--- Mixtape/datasets/__init__.py
+++ Mixtape/datasets/__init__.py
@@ -1,5 +1,6 @@
 from .base import get_data_home
 from .base import clear_data_home
+from .brownian1d import load_doublewell
 from .alanine_dipeptide import fetch_alanine_dipeptide
 from .met_enkephalin import fetch_met_enkephalin
 
@@ -8,4 +9,5 @@ __all__ = [
     'clear_data_home',
     'fetch_alanine_dipeptide',
     'fetch_met_enkephalin',
+    'load_doublewell',
 ]
