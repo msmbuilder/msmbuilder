a534e7dba54a8eecadacfcc9139dc4057101c473
Robert McGibbon <rmcgibbo@gmail.com>
2014-05-19 13:58:59 -0700

    analytic results for doublwell

--- Mixtape/datasets/brownian1d.py
+++ Mixtape/datasets/brownian1d.py
@@ -90,15 +90,16 @@ load_doublewell.__doc__ += DOUBLEWELL_DESCRIPTION
 # Internal functions
 #-----------------------------------------------------------------------------
 
+DOUBLEWELL_GRAD_POTENTIAL = lambda x : -2 * np.sin(2 * x)
+
 def _simulate_doublewell(random):
     # DO NOT CHANGE THESE CONSTANTS WITHOUT UPDATING THE
     # "DOUBLEWELL_DESCRIPTION" VARIABLE
     x0 = 0
     n_steps = 1e5
     n_trajectories = 10
-    grad_potential = lambda x : -2 * np.sin(2 * x)
 
-    trajectories = [_propagate1d(x0, n_steps, grad_potential, random,
+    trajectories = [_propagate1d(x0, n_steps, DOUBLEWELL_GRAD_POTENTIAL, random,
                                  bc_min=-np.pi, bc_max=np.pi).reshape(-1, 1)
                     for i in range(n_trajectories)]
     return trajectories
@@ -129,3 +130,32 @@ def _propagate1d(x0, n_steps, grad_potential, random, bc_min=None, bc_max=None):
 
     print('%d steps/s' % (n_steps / (time.time() - start)))
     return x
+
+
+def doublewell_eigs(n_grid, lag_time=1, grad_potential=DOUBLEWELL_GRAD_POTENTIAL):
+    """Analytic eigenvalues/eigenvectors for the doublwell system.
+
+    """
+    import scipy.linalg
+    ONE_OVER_SQRT_2PI = 1.0 / (np.sqrt(2*np.pi))
+    normalpdf = lambda x : ONE_OVER_SQRT_2PI * np.exp(-0.5 * (x*x))
+
+    grid = np.linspace(-np.pi, np.pi, n_grid)
+    width = grid[1]-grid[0]
+    transmat = np.zeros((n_grid, n_grid))
+    for i, x_i in enumerate(grid):
+        for offset in range(-(n_grid-1), n_grid):
+            x_j = x_i + (offset * width)
+            j = _reflect_boundary_conditions(i+offset, 0, n_grid-1)
+
+            # What is the probability of going from x_i to x_j in one step?
+            diff = (x_j - x_i + DT * grad_potential(x_i)) / DT_SQRT_2D
+            transmat[i, j] += normalpdf(diff)
+        transmat[i, :] =  transmat[i, :] / np.sum(transmat[i, :])
+
+
+    transmat = np.linalg.matrix_power(transmat, lag_time)
+    u, v = scipy.linalg.eig(transmat)
+    order = np.argsort(np.real(u))[::-1]
+
+    return np.real_if_close(u[order]), np.real_if_close(v[:, order])
