3acf8eab016a9afedff306e1f49683ce37a0dcbe
Robert McGibbon <rmcgibbo@gmail.com>
2014-05-19 14:32:54 -0700

    imports

--- Mixtape/datasets/__init__.py
+++ Mixtape/datasets/__init__.py
@@ -3,11 +3,13 @@ from .base import clear_data_home
 from .brownian1d import load_doublewell
 from .alanine_dipeptide import fetch_alanine_dipeptide
 from .met_enkephalin import fetch_met_enkephalin
+from .fs_peptide import fetch_fs_peptide
 
 __all__ = [
     'get_data_home',
     'clear_data_home',
+    'load_doublewell',
     'fetch_alanine_dipeptide',
     'fetch_met_enkephalin',
-    'load_doublewell',
+    'fetch_fs_peptide',
 ]
