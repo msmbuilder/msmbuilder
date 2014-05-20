Fixed some numerical issues in log-det penalty

Mixtape/mslds_solver.py
@@ -11,6 +11,7 @@ from mixtape.mslds_solvers.sparse_sdp.objectives import grad_log_det_tr
 from mixtape.mslds_solvers.sparse_sdp.general_sdp_solver \
         import GeneralSolver
 from mixtape.mslds_solvers.sparse_sdp.utils import get_entries, set_entries
+from mixtape.utils import bcolors
 
 class MetastableSwitchingLDSSolver(object):
     """
@@ -70,6 +71,29 @@ class MetastableSwitchingLDSSolver(object):
                 covars.append(np.zeros(np.shape(obsmean)))
         return covars
 
+    def print_aux_matrices(self, Bs, Cs, Es, Ds, Fs):
+        display_string = """
+        ++++++++++++++++++++++++++
+        Current Aux Matrices.
+        ++++++++++++++++++++++++++
+        """
+        for i in range(self.n_components):
+            B, C, D, E, F = Bs[i], Cs[i], Ds[i], Es[i], Fs[i]
+            display_string += ("""
+            --------
+            State %d
+            --------
+            """ % i)
+            display_string += (("\nBs[%d]:\n"%i + str(Bs[i]) + "\n")
+                             + ("\nCs[%d]:\n"%i + str(Cs[i]) + "\n")
+                             + ("\nDs[%d]:\n"%i + str(Ds[i]) + "\n")
+                             + ("\nEs[%d]:\n"%i + str(Es[i]) + "\n")
+                             + ("\nFs[%d]:\n"%i + str(Fs[i]) + "\n"))
+        display_string = (bcolors.WARNING + display_string
+                            + bcolors.ENDC)
+        print(display_string)
+
+
     def means_update(self, stats):
         means = (stats['obs']) / (stats['post'][:, np.newaxis])
         return means
@@ -77,6 +101,7 @@ class MetastableSwitchingLDSSolver(object):
     def AQb_update(self, As, Qs, bs, means, covars, stats):
         Bs, Cs, Es, Ds, Fs = compute_aux_matrices(self.n_components,
                 self.n_features, As, bs, covars, stats)
+        self.print_aux_matrices(Bs, Cs, Es, Ds, Fs)
         A_upds, Q_upds, b_upds = [], [], []
 
         for i in range(self.n_components):
@@ -92,12 +117,17 @@ class MetastableSwitchingLDSSolver(object):
         return A_upds, Q_upds, b_upds
 
 def AQb_solve(dim, A, Q, Qinv, mu, B, C, D, Dinv, E, F):
+    print "mu: ", mu
     # Should this be iterated for biconvex solution? Yes. Need to fix.
-    Q = Q_solve(dim, A, D, Dinv, F)
-    A = A_solve(dim, B, C, D, Dinv, E, Q, Qinv, mu)
-    b = b_solve(dim, A, mu)
-    print "A_upd: ", A
+    Q_upd = Q_solve(dim, A, D, Dinv, F)
+    if Q_upd != None:
+        Q = Q_upd
     print "Q_upd: ", Q
+    A_upd = A_solve(dim, B, C, D, Dinv, E, Q, Qinv, mu)
+    if A_upd != None:
+        A = A_upd
+    print "A_upd: ", A
+    b = b_solve(dim, A, mu)
     print "b_upd: ", b
     return A, Q, b
 
@@ -151,12 +181,12 @@ def compute_aux_matrices(n_components, n_features, As, bs, covars, stats):
     return Bs, Cs, Es, Ds, Fs
 
 def b_solve(n_features, A, mu):
-    print "mu:\n", mu
+    #print "mu:\n", mu
     b = np.dot(np.eye(n_features) - A, mu)
-    print "b:\n", b
-    import pdb
-    pdb.set_trace()
-    return b
+    #print "b:\n", b
+    #return b
+    # b = mu since constraint A mu == 0
+    return mu
 
 def A_solve(block_dim, B, C, D, Dinv, E, Q, Qinv, mu):
     """
@@ -202,7 +232,7 @@ def A_solve(block_dim, B, C, D, Dinv, E, Q, Qinv, mu):
     g.save_constraints(obj, grad_obj, As, bs, Cs, ds,
             Fs, gradFs, Gs, gradGs)
     (L, U, X, succeed) = g.solve(N_iter, tol,
-            interactive=False, disp=True, verbose=False, debug=True)
+            interactive=False, disp=True, verbose=False, debug=False)
     if succeed:
         A_1 = get_entries(X, A_1_cds)
         A_T_1 = get_entries(X, A_T_1_cds)
@@ -211,7 +241,8 @@ def A_solve(block_dim, B, C, D, Dinv, E, Q, Qinv, mu):
         A = (A_1 + A_T_1 + A_2 + A_T_2) / 4.
         return A
 
-def Q_solve(block_dim, A, D, Dinv, F):
+def Q_solve(block_dim, A, D, Dinv, F, interactive=False, disp=True,
+        verbose=False, debug=False, Rs=[10, 100, 1000]):
     """
     Solves Q optimization.
 
@@ -242,8 +273,8 @@ def Q_solve(block_dim, A, D, Dinv, F):
         return grad_log_det_tr(scale*X, F)
     g.save_constraints(obj, grad_obj, As, bs, Cs, ds,
             Fs, gradFs, Gs, gradGs)
-    (L, U, X, succeed) = g.solve(N_iter, tol, interactive=False,
-            disp=True, verbose=False)
+    (L, U, X, succeed) = g.solve(N_iter, tol, interactive=interactive,
+            disp=disp, verbose=verbose, debug=debug, Rs=Rs)
     if succeed:
         R_1 = scale*get_entries(X, R_1_cds)
         R_2 = scale*get_entries(X, R_2_cds)

Mixtape/mslds_solvers/sparse_sdp/objectives.py
@@ -61,10 +61,15 @@ def log_det_tr(X, B):
     R2 = get_entries(X, R_2_cds)
     try:
         val1 = -np.log(np.linalg.det(R1)) + np.trace(np.dot(R1, B))
-        val2 = -np.log(np.linalg.det(R2)) + np.trace(np.dot(R2, B))
-        val = val1 + val2
     except FloatingPointError:
-        return -np.inf
+        if np.linalg.det(R1) < np.finfo(np.float).eps:
+            val1 = np.inf
+    try:
+        val2 = -np.log(np.linalg.det(R2)) + np.trace(np.dot(R2, B))
+    except FloatingPointError:
+        if np.linalg.det(R2) < np.finfo(np.float).eps:
+            val2 = np.inf
+    val = val1 + val2
     return val
 
 # grad - log det R = -R^{-1} = -Q (see Boyd and Vandenberge, A4.1)

Mixtape/mslds_solvers/sparse_sdp/penalties.py
@@ -92,8 +92,12 @@ def log_sum_exp_penalty(X, M, As, bs, Cs, ds, Fs, Gs):
     retval = 0.
     m, n, p, q = len(As), len(Cs), len(Fs), len(Gs)
     if m + n + p + q > 0:
-        retval = scipy.misc.logsumexp(M*np.array(pens), axis=0)
-        retval = -(1.0/M) * retval
+        try:
+            retval = scipy.misc.logsumexp(M*np.array(pens), axis=0)
+            retval = -(1.0/M) * retval
+        except FloatingPointError:
+            if np.amax(pens) == np.inf:
+                return -np.inf
     return retval
 
 def log_sum_exp_grad_penalty(X, M, As, bs, Cs, ds, Fs, gradFs, Gs, gradGs):

test/test_mslds_mstep.py
@@ -41,12 +41,50 @@ def test_A_solve_plusmin():
     B = np.array([[1238.916]])
     C = np.array([[1225.025]])
     D = np.array([[.0204]])
-    Dinv = np.array([[49.02]])
     E = np.array([[48.99]])
     Q = np.array([[.02]])
+    Dinv = np.array([[49.02]])
     Qinv = np.array([[48.99]])
     mu = np.array([[1.]])
     A_solve(block_dim, B, C, D, Dinv, E, Q, Qinv, mu)
+
+def test_A_solve_plusmin_2():
+    block_dim = 1
+    B = np.array([[ 965.82431552]])
+    C = np.array([[ 950.23843989]])
+    D = np.array([[ 0.02430409]])
+    E = np.array([[ 974.49540394]])
+    F = np.array([[ 24.31867657]])
+    Q = np.array([[ 0.02596519]])
+    mu = np.array([[ -1.]])
+    Dinv = np.linalg.inv(D)
+    Qinv = np.linalg.inv(Q)
+    A_solve(block_dim, B, C, D, Dinv, E, Q, Qinv, mu)
+
+def test_Q_solve_muller():
+    block_dim = 2
+    np.set_printoptions(precision=2)
+
+    A = np.zeros((block_dim, block_dim))
+    B = np.array([[208.27749525,  -597.11827148],
+                   [ -612.99179464, 1771.25551671]])
+
+    C = np.array([[202.83070879, -600.32796941],
+                   [-601.76432584, 1781.07130791]])
+
+    D = np.array([[0.00326556, 0.00196009],
+                   [0.00196009, 0.00322879]])
+
+    E = np.array([[205.80695137, -599.79918374],
+                  [-599.79918374, 1782.52514543]])
+
+    F = np.array([[2.62197238, 1.58163533],
+                  [1.58163533, 2.58977211]])
+    Dinv = np.linalg.inv(D)
+    Q_solve(block_dim, A, D, Dinv, F, disp=True, debug=True,
+            verbose=True, Rs=[100])
+
+
 
 def test_Q_solve_plusmin():
     block_dim = 1