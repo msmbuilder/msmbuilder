2edb2bd0b085d4027fa47b36dc4bd7e9b073b79b
Robert McGibbon <rmcgibbo@gmail.com>
2014-06-06 14:29:34 -0700

    add docstring

--- Mixtape/cluster/_commonc.pyx
+++ Mixtape/cluster/_commonc.pyx
@@ -6,9 +6,8 @@ from blas cimport *
 from libc.math cimport sqrt
 cimport cython
 cimport numpy as np
-from libc.string cimport memcpy
 import numpy as np
-from numpy import zeros, ones, array
+from numpy import zeros
 import scipy.linalg.blas
 
 __all__ = ['_kcenters_euclidean']
@@ -33,11 +32,30 @@ cdef sdot_t *sdot = <sdot_t*>f2py_pointer(scipy.linalg.blas.sdot._cpointer)
 @cython.boundscheck(False)
 @cython.wraparound(False)
 @cython.cdivision(True)
-cpdef _assign_labels_array(real[:, ::1] X,
-                           real[:, ::1] centers,
-                           long long[::1] labels,
-                           real[::1] distances):
+cpdef np.float64_t _assign_labels_array(real[:, ::1] X,
+                                        real[:, ::1] centers,
+                                        long long[::1] labels,
+                                        real[::1] distances):
     """Compute label assignment and inertia
+
+    Parameters
+    ----------
+    X : 2d array [INPUT]
+        The data array
+    centers : 2d array [INPUT]
+        The array with the cluster centers
+    labels : 1d array [OUTPUT]
+        The output labels for each data point (integers from 0 to n_clusters-1) will
+        be written into this array
+    distances : 1d array [OUTPUT]
+        If distances is supplied and is an array of length == len(X), the distances
+        from each X to its assigned cluster center will be written here
+
+    Returns
+    --------
+    intertia : double
+        The sum of the squares of the distance from each point to its assigned
+        center
     """
     cdef:
         int n_clusters = centers.shape[0]
