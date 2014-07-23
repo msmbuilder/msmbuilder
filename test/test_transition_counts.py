5c931a2c406528a63d3efe2a036d967985ddd0c3
Robert McGibbon <rmcgibbo@gmail.com>
2014-07-23 14:28:13 -0700

    test with strings

--- test/test_transition_counts.py
+++ test/test_transition_counts.py
@@ -1,15 +1,24 @@
 import numpy as np
 from mixtape.markovstatemodel import _transition_counts
 
+
 def test_1():
     with np.testing.assert_raises(ValueError):
         _transition_counts([1,2,3])
+
 
 def test_2():
     c, m = _transition_counts([np.arange(10)])
     np.testing.assert_array_equal(c, np.eye(10, k=1))
     assert m.keys() == range(10)
     assert m.values() == range(10)
-    
 
 
+def test_3():
+    c, m = _transition_counts([['alpha', 'b', 'b', 'b', 'c']])
+    np.testing.assert_array_equal(c, 1.0 * np.array([
+        [0, 1, 0],
+        [0, 2, 1],
+        [0, 0, 0]
+    ]))
+    assert m == {'alpha': 0, 'b': 1, 'c': 2}
