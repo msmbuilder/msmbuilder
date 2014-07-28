51ecc49095ce33fa123ac49de57773da32f0a1cb
Robert McGibbon <rmcgibbo@gmail.com>
2014-07-27 17:03:06 -0700

    more

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -qq -y g++ gfortran valgrind csh
 sudo apt-get install -qq -y g++-multilib gcc-multilib
-wget http://repo.continuum.io/miniconda/Miniconda-3.0.5-Linux-x86_64.sh
+wget http://repo.continuum.io/miniconda/Miniconda-3.5.5-Linux-x86_64.sh
 bash Miniconda-3.0.5-Linux-x86_64.sh -b
 PIP_ARGS="-U"
 
@@ -10,6 +10,6 @@ conda update --yes conda
 conda create --yes -n ${python} --file devtools/ci/requirements-conda-${python}.txt
 conda config --add channels http://conda.binstar.org/omnia
 source activate $python
-conda install --yes mdtraj scikit-learn sphinx openmm ipython matplotlib
+conda install --yes mdtraj scikit-learn sphinx openmm ipython-notebook matplotlib
 conda list -e
 $HOME/miniconda/envs/${python}/bin/pip install $PIP_ARGS -r devtools/ci/requirements-${python}.txt
