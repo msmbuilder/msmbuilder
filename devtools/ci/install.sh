3d48a85013a812396f37dab94cced05971fcac70
Robert McGibbon <rmcgibbo@gmail.com>
2014-07-27 17:08:17 -0700

    fix

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -1,7 +1,7 @@
 sudo apt-get install -qq -y g++ gfortran valgrind csh
 sudo apt-get install -qq -y g++-multilib gcc-multilib
 wget http://repo.continuum.io/miniconda/Miniconda-3.5.5-Linux-x86_64.sh
-bash Miniconda-3.0.5-Linux-x86_64.sh -b
+bash Miniconda-3.5.5-Linux-x86_64.sh -b
 PIP_ARGS="-U"
 
 export PATH=$HOME/miniconda/bin:$PATH
