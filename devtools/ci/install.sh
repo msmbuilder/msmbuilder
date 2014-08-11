dd5a4f5c30a814067bbef712171e0ef797bb9fd8
Robert McGibbon <rmcgibbo@gmail.com>
2014-08-11 10:16:22 -0700

    Faster

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -1,3 +1,4 @@
+sudo apt-get update
 sudo apt-get install -qq -y g++ gfortran valgrind csh
 sudo apt-get install -qq -y g++-multilib gcc-multilib
 wget http://repo.continuum.io/miniconda/Miniconda-3.5.5-Linux-x86_64.sh
@@ -7,10 +8,9 @@ PIP_ARGS="-U"
 export PATH=$HOME/miniconda/bin:$PATH
 
 conda update --yes conda
-conda create --yes -n ${python} --file devtools/ci/requirements-conda-${python}.txt
 conda config --add channels http://conda.binstar.org/omnia
+conda create --yes -n ${python} --file devtools/ci/requirements-conda-${python}.txt
 source activate $python
-conda install --yes mdtraj scikit-learn sphinx openmm ipython-notebook matplotlib
 conda list -e
 $HOME/miniconda/envs/${python}/bin/pip install $PIP_ARGS -r devtools/ci/requirements-${python}.txt
 
