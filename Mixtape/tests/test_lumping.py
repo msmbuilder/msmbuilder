d9c000745da5c4b8f524ae78d673cdfe8fa7e924
Robert McGibbon <rmcgibbo@gmail.com>
2014-11-15 17:52:25 -0800

    (WIP) simplify the PCCA tests

--- Mixtape/tests/test_lumping.py
+++ Mixtape/tests/test_lumping.py
@@ -2,70 +2,67 @@ from __future__ import print_function
 
 import numpy as np
 from mdtraj.testing import eq
-import sklearn.pipeline
+from sklearn.pipeline import Pipeline
 
 from mixtape.lumping import PCCA, PCCAPlus
 from mixtape.msm import MarkovStateModel
 
+def _metastable_system():
+    # Make a simple dataset with four states, where there are 2 obvious
+    # macrostate. States 0 and 1 interconvert, states 2 and 3 interconvert.
+    n = 250
+    random = np.random.RandomState(1)
+    assignments = np.concatenate([
+        0 + random.randint(2, size=n),  # states 0, 1 interconverting
+        2 + random.randint(2, size=n),  # states 2, 3 intercoverting
+        0 + random.randint(2, size=n),  # states 0, 1 interconverting
+        2 + random.randint(2, size=n)   # states 2, 3 interconverting
+    ])
+    
+    # the true (2-state) macrostate assignments
+    macro_assignments = np.concatenate([
+        np.zeros(n, dtype=int),
+        np.ones(n, dtype=int),
+        np.zeros(n, dtype=int),
+        np.ones(n, dtype=int)
+    ])
+
+    return assignments, macro_assignments
+
 
 def test_pcca_1():
-    # Make a simple dataset with four states, where there are 2 obvious macrostate basins--the remaining states interconvert quickly
-    n_frames = 10000
-    chunk = np.zeros(n_frames, 'int')
-    rnd = lambda: np.random.randint(0, 2, n_frames)  # Generates random noise states within each basin
-    # States 0 and 1 interconvert, states 2 and 3 interconvert.  
-    assignments = [np.hstack((chunk + rnd(), chunk + 2  + rnd())), np.hstack((chunk + 2 + rnd(), chunk + rnd()))]
-
-    pcca = PCCA(2)
-    macro_msm = MarkovStateModel()
-
-    pipeline = sklearn.pipeline.Pipeline([("pcca", pcca), ("macro_msm", macro_msm)])
-    macro_assignments = pipeline.fit_transform(assignments)
-
-    # Now let's make make the output assignments start with zero at the first position.
-    i0 = macro_assignments[0][0]
-    if i0 == 1:
-        for m in macro_assignments:
-            m *= -1
-            m += 1
-
-    eq(macro_assignments[0], np.hstack((chunk, chunk + 1)))
-    eq(macro_assignments[1], np.hstack((chunk + 1, chunk)))
+    assignments, ref_macrostate_assignments = _metastable_system()
+    pipeline = Pipeline([
+        ('msm', MarkovStateModel()),
+        ('pcca+', PCCA(2))
+    ])
+    macro_assignments = pipeline.fit_transform(assignments)[0]
+    
+    # we need to consider any permutation of the state labels when we
+    # test for equality. Since it's only a 2-state that's simple using
+    # the logical_not to flip the assignments.
+    assert (np.all(macro_assignments == ref_macrostate_assignments) or
+            np.all(macro_assignments == np.logical_not(ref_macrostate_assignments)))
 
 
-def test_pccaplus_1():
-    # Make a simple dataset with four states, where there are 2 obvious macrostate basins--the remaining states interconvert quickly
-    n_frames = 10000
-    chunk = np.zeros(n_frames, 'int')
-    rnd = lambda : np.random.randint(0, 2, n_frames)  # Generates random noise states within each basin
-    # States 0 and 1 interconvert, states 2 and 3 interconvert.  
-    assignments = [np.hstack((chunk + rnd(), chunk + 2  + rnd())), np.hstack((chunk + 2 + rnd(), chunk + rnd()))]
-
-    pcca = PCCAPlus(2)
-    macro_msm = MarkovStateModel()
-
-    pipeline = sklearn.pipeline.Pipeline([("pcca", pcca), ("macro_msm", macro_msm)])
-    macro_assignments = pipeline.fit_transform(assignments)
-
-    # Now let's make make the output assignments start with zero at the first position.
-    i0 = macro_assignments[0][0]
-    if i0 == 1:
-        for m in macro_assignments:
-            m *= -1
-            m += 1
-
-    eq(macro_assignments[0], np.hstack((chunk, chunk + 1)))
-    eq(macro_assignments[1], np.hstack((chunk + 1, chunk)))
+def test_pcca_1():
+    assignments, ref_macrostate_assignments = _metastable_system()
+    pipeline = Pipeline([
+        ('msm', MarkovStateModel()),
+        ('pcca+', PCCAPlus(2))
+    ])
+    macro_assignments = pipeline.fit_transform(assignments)[0]
+    print(macro_assignments)
+    
+    # we need to consider any permutation of the state labels when we
+    # test for equality. Since it's only a 2-state that's simple using
+    # the logical_not to flip the assignments.
+    assert (np.all(macro_assignments == ref_macrostate_assignments) or
+            np.all(macro_assignments == np.logical_not(ref_macrostate_assignments)))
 
 
 def test_from_msm():
-    # Make a simple dataset with four states, where there are 2 obvious macrostate basins--the remaining states interconvert quickly
-    n_frames = 10000
-    chunk = np.zeros(n_frames, 'int')
-    rnd = lambda : np.random.randint(0, 2, n_frames)  # Generates random noise states within each basin
-    # States 0 and 1 interconvert, states 2 and 3 interconvert.  
-    assignments = [np.hstack((chunk + rnd(), chunk + 2  + rnd())), np.hstack((chunk + 2 + rnd(), chunk + rnd()))]
-    
+    assignments, _ = _metastable_system()
     msm = MarkovStateModel()
     msm.fit(assignments)
     pcca = PCCA.from_msm(msm, 2)
