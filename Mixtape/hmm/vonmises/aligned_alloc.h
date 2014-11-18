169e5c31d49aece9ef6b8cb9dd32853101426ee2
Robert McGibbon <rmcgibbo@gmail.com>
2014-11-17 16:04:00 -0800

    fix?

--- Mixtape/hmm/vonmises/aligned_alloc.h
+++ Mixtape/hmm/vonmises/aligned_alloc.h
@@ -1,11 +1,13 @@
 #ifndef ALIGNED_ALLOC
 #define ALIGNED_ALLOC
+#include <stdlib.h>
+#include <malloc.h>
 
 void * malloc_simd(const size_t size) 
 {
 	const size_t alignment = 16;
 #ifdef __linux__
-	return aligned_alloc(alignment, size);
+	return memalign(alignment, size);
 #endif
 #ifdef WIN32
 	return _aligned_malloc(size, alignment);
