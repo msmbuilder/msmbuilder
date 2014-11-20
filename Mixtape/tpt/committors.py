df041f1f8aff3ba17fcc799e9aa330f8ab72ec48
Christian Schwantes <schwancr@stanford.edu>
2014-11-20 11:29:36 -0800

    translated hubs.py and cleaned things up a bit. I ended up putting the conditional committor calculation in committors.py

--- Mixtape/tpt/committors.py
+++ Mixtape/tpt/committors.py
@@ -93,3 +93,113 @@ def calculate_committors(sources, sinks, msm):
     assert np.all(committors >= (0.0 - epsilon))
 
     return committors
+
+
+def calculate_conditional_committors(source, sink, waypoint, msm):
+"""
+    Calculate the fraction of times a walker on `tprob` going from `sources`
+    to `sinks` will travel through the set of states `waypoints` en route.
+
+    Computes the conditional committors q^{ABC^+} and uses them to find the
+    fraction of paths mentioned above. The conditional committors can be
+
+    Note that in the notation of Dickson et. al. this computes h_c(A,B), with
+        sources   = A
+        sinks     = B
+        waypoint  = C
+
+    Parameters
+    ----------
+    waypoint : int
+        The index of the intermediate state
+    source : int
+        The index of the source state
+    sink : int
+        The index of the sink state
+    msm : mixtape.MarkovStateModel
+        MSM to analyze.
+
+    Returns
+    -------
+    cond_committors : np.ndarray
+        Conditional committors, i.e. the probability of visiting
+        a waypoint when on a path between source and sink.
+
+    See Also
+    --------
+    hubs.calculate_fraction_visits : function
+        Calculate the fraction of visits to a waypoint from a given
+        source to a sink.
+    hubs.calculate_hub_score : function
+        Compute the 'hub score', the weighted fraction of visits for an
+        entire network.
+    hubs.calculate_all_hub_scores : function
+        Wrapper to compute all the hub scores in a network.
+
+    Notes
+    -----
+    Employs dense linear algebra,
+      memory use scales as N^2
+      cycle use scales as N^3
+
+    References
+    ----------
+    ..[1] Dickson & Brooks (2012), J. Chem. Theory Comput.,
+          Article ASAP DOI: 10.1021/ct300537s
+    """
+
+    tprob = msm.transmat_
+    n_states = msm.n_states_
+
+    # typecheck
+    for data in [source, sink, waypoint]:
+        if not isinstance(data, int):
+            raise ValueError("source, sink, and waypoint must be integers.")
+            
+    if (source == waypoint) or (sink == waypoint) or (sink == source):
+        raise ValueError('source, sink, waypoint must all be disjoint!')
+
+    committors = calculate_committors([source], [sink], msm)
+
+    # permute the transition matrix into cannonical form - send waypoint the the
+    # last row, and source + sink to the end after that
+    Bsink_indices = [source, sink, waypoint]
+    perm = np.array([i for i in xrange(n_states) if not i in Bsink_indices])
+    perm = np.concatenate([perm, Bsink_indices])
+    permuted_tprob = tprob[perm, :][:, perm]
+
+    ## OLD CODE (to be deleted)
+    # I think the list comp. is easier to understand
+    #perm = np.arange(N)
+    #perm = np.delete(perm, Bsink_indices)
+    #perm = np.append(perm, Bsink_indices)
+
+    # this function is unnecessary. Also it actullay computes a permutation
+    # matrix and does a bunch of matrix multiplies. The slicing above does
+    # the same thing but in a more understandable way.
+    #T = MSMLib.permute_mat(tprob, perm)
+
+    # extract P, R
+    n = n_states - len(Bsink_indices)
+    P = permuted_tprob[:n, :n]
+    R = permuted_tprob[:n, n:]
+
+    # calculate the conditional committors ( B = N*R ), B[i,j] is the prob
+    # state i ends in j, where j runs over the source + sink + waypoint
+    # (waypoint is position -1)
+    B = np.dot(np.linalg.inv(np.eye(n) - P), R)
+
+    # add probs for the sinks, waypoint / b[i] is P( i --> {C & not A, B} )
+    b = np.append(B[:, -1].flatten(), [0.0] * (len(Bsink_indices) - 1) + [1.0])
+    cond_committors = b * committors[waypoint]
+
+    epsilon = 1e-6  # some numerical give, hard-coded
+    assert cond_committors.shape == (N,)
+    assert np.all(cond_committors <= 1.0 + epsilon)
+    assert np.all(cond_committors >= 0.0 - epsilon)
+    assert np.all(cond_committors <= committors[perm] + epsilon)
+
+    # get the original order
+    cond_committors = cond_committors[np.argsort(perm)] 
+
+    return cond_committors
--- Mixtape/tpt/hubs.py
+++ Mixtape/tpt/hubs.py
@@ -21,11 +21,7 @@ References
 """
 from __future__ import print_function, division, absolute_import
 import numpy as np
-import scipy.sparse
 
-from msmbuilder import MSMLib
-from msmbuilder import msm_analysis
-from msmbuilder.utils import deprecated
 import itertools
 import copy
 
@@ -39,7 +35,7 @@ logger = logging.getLogger(__name__)
 # Functions for computing hub scores, conditional committors, and
 # related quantities
 #
-def calculate_fraction_visits(tprob, waypoint, source, sink, return_cond_Q=False):
+def calculate_fraction_visits(source, sink, waypoint, msm):
     """
     Calculate the fraction of times a walker on `tprob` going from `sources`
     to `sinks` will travel through the set of states `waypoints` en route.
@@ -54,38 +50,31 @@ def calculate_fraction_visits(tprob, waypoint, source, sink, return_cond_Q=False
 
     Parameters
     ----------
-    tprob : matrix
-        The transition probability matrix
+    source : int
+        The index of the source state
+    sink : int
+        The index of the sink state
     waypoint : int
         The index of the intermediate state
-    sources : nd_array, int or int
-        The indices of the source state(s)
-    sinks : nd_array, int or int
-        The indices of the sink state(s)
-    return_cond_Q : bool
-        Whether or not to return the conditional committors
+    msm : mixtape.MarkovStateModel
+        MSM to analyze.
 
     Returns
     -------
-    fraction_paths : float
+    fraction_visited : float
         The fraction of times a walker going from `sources` -> `sinks` stops
         by `waypoints` on its way.
-    cond_Q : nd_array, float (optional)
-        Optionally returned (`return_cond_Q`)
 
     See Also
     --------
-    calculate_hub_score : function
+    committors.calculate_conditional_committors
+        Calculate the probability of visiting a waypoint while on a path
+        between a source and sink.
+    hubs.calculate_hub_score : function
         Compute the 'hub score', the weighted fraction of visits for an
         entire network.
-    calculate_all_hub_scores : function
+    hubs.calculate_all_hub_scores : function
         Wrapper to compute all the hub scores in a network.
-
-    Notes
-    -----
-    Employs dense linear algebra,
-      memory use scales as N^2
-      cycle use scales as N^3
 
     References
     ----------
@@ -93,78 +82,26 @@ def calculate_fraction_visits(tprob, waypoint, source, sink, return_cond_Q=False
           Article ASAP DOI: 10.1021/ct300537s
     """
 
-    # do some typechecking - we need to be sure that the lumped sources are in
-    # the second to last row, and the lumped sinks are in the last row
-    # check `tprob`
-    msm_analysis.check_transition(tprob)
-    if type(tprob) != np.ndarray:
-        try:
-            tprob = tprob.todense()
-        except AttributeError as e:
-            raise TypeError('Argument `tprob` must be convertable to a dense'
-                            'numpy array. \n%s' % e)
+    tprob = msm.transmat_
+    n_states = msm.n_states_
 
-    # typecheck
-    for data in [source, sink, waypoint]:
-        if type(data) == int:
-            pass
-        elif hasattr(data, 'len'):
-            if len(data) == 1:
-                data = data[0]
-        else:
-            raise TypeError('Arguments source/sink/waypoint must be an int')
+    # EFFICIENCY ALERT:
+    # we could allow all of these functions to pass committors if they've
+    # already been calculated, but I think it's so fast that we don't need to
+    committors = tpt.committors.calculate_committors([source], [sink], msm)
+    cond_committors = tpt.committors.calculate_conditional_committors([source], [sink], msm)
 
-    if (source == waypoint) or (sink == waypoint) or (sink == source):
-        raise ValueError('source, sink, waypoint must all be disjoint!')
+    # OLD CODE: The transition matrix is permuted so that the last three are source, sink, waypoint
+    #fraction_paths = np.sum(permuted_tprob[-3, :] * cond_committors) / np.sum(permuted_tprob[-3, :] * committors[perm])
 
-    N = tprob.shape[0]
-    Q = calculate_committors([source], [sink], tprob)
+    fraction_visited = np.float(tprob[source, :].dot(cond_committors)) / np.float(tprob[source, :].dot(committors))
 
-    # permute the transition matrix into cannonical form - send waypoint the the
-    # last row, and source + sink to the end after that
-    Bsink_indices = [source, sink, waypoint]
-    perm = np.arange(N)
-    perm = np.delete(perm, Bsink_indices)
-    perm = np.append(perm, Bsink_indices)
-    T = MSMLib.permute_mat(tprob, perm)
-
-    # extract P, R
-    n = N - len(Bsink_indices)
-    P = T[:n, :n]
-    R = T[:n, n:]
-
-    # calculate the conditional committors ( B = N*R ), B[i,j] is the prob
-    # state i ends in j, where j runs over the source + sink + waypoint
-    # (waypoint is position -1)
-    B = np.dot(np.linalg.inv(np.eye(n) - P), R)
-    # Not sure if this is sparse or not...
-
-    # add probs for the sinks, waypoint / b[i] is P( i --> {C & not A, B} )
-    b = np.append(B[:, -1].flatten(), [0.0] * (len(Bsink_indices) - 1) + [1.0])
-    cond_Q = b * Q[waypoint]
-
-    epsilon = 1e-6  # some numerical give, hard-coded
-    assert cond_Q.shape == (N,)
-    assert np.all(cond_Q <= 1.0 + epsilon)
-    assert np.all(cond_Q >= 0.0 - epsilon)
-    assert np.all(cond_Q <= Q[perm] + epsilon)
-
-    # finally, calculate the fraction of paths h_C(A,B) (eq. 7 in [1])
-    fraction_paths = np.sum(T[-3, :] * cond_Q) / np.sum(T[-3, :] * Q[perm])
-
-    assert fraction_paths <= 1.0
-    assert fraction_paths >= 0.0
-
-    if return_cond_Q:
-        cond_Q = cond_Q[np.argsort(perm)]  # put back in orig. order
-        return fraction_paths, cond_Q
-    else:
-        return fraction_paths
+    return fraction_visited
 
 
-def calculate_hub_score(tprob, waypoint):
+def calculate_hub_score(msm, waypoint):
     """
-    Calculate the hub score for the states `waypoint`.
+    Calculate the hub score for a single `waypoint`.
 
     The "hub score" is a measure of how well traveled a certain state or
     set of states is in a network. Specifically, it is the fraction of
@@ -174,70 +111,48 @@ def calculate_hub_score(tprob, waypoint):
 
     Parameters
     ----------
-    tprob : matrix
-        The transition probability matrix
-    waypoints : int
-        The indices of the intermediate state(s)
+    msm : mixtape.MarkovStateModel
+        MSM to analyze
+    waypoint : int
+        The index of the intermediate state
 
     Returns
     -------
-    Hc : float
-        The hub score for the state composed of `waypoints`
+    hub_score : float
+        The hub score for the waypoint
 
     See Also
     --------
-    calculate_fraction_visits : function
-        Calculate the fraction of times a state is visited on pathways going
-        from a set of "sources" to a set of "sinks".
-    calculate_all_hub_scores : function
+    hubs.calculate_all_hub_scores : function
         A more efficient way to compute the hub score for every state in a
         network.
 
-    Notes
-    -----
-    Employs dense linear algebra,
-      memory use scales as N^2
-      cycle use scales as N^5
-
     References
     ----------
     ..[1] Dickson & Brooks (2012), J. Chem. Theory Comput.,
         Article ASAP DOI: 10.1021/ct300537s
     """
 
-    msm_analysis.check_transition(tprob)
+    tprob = msm.transmat_
+    n_states = msm.n_states_
+    if not isinstance(waypoint, int):
+        raise ValueError("waypoint (%s) must be an int" % str(waypoint))
 
-    # typecheck
-    if type(waypoint) != int:
-        if hasattr(waypoint, '__len__'):
-            if len(waypoint) == 1:
-                waypoint = waypoint[0]
-            else:
-                raise ValueError('Must pass waypoints as int or list/array of ints')
-        else:
-            raise ValueError('Must pass waypoints as int or list/array of ints')
-
-    # find out which states to include in A, B (i.e. everything but C)
-    N = tprob.shape[0]
-    states_to_include = list(range(N))
-    states_to_include.remove(waypoint)
+    other_states = (i for i in xrange(n_states) if i != waypoint)
 
     # calculate the hub score
-    Hc = 0.0
-    for s1 in states_to_include:
-        for s2 in states_to_include:
-            if (s1 != s2) and (s1 != waypoint) and (s2 != waypoint):
-                Hc += calculate_fraction_visits(tprob, waypoint,
-                                                s1, s2, return_cond_Q=False)
+    hub_score = 0.0
+    for (source, sink) in itertools.permutations(other_states, 2):
+        hub_score += calculate_fraction_visits(source, sink, waypoint, msm)
 
-    Hc /= ((N - 1) * (N - 2))
+    hub_score /= float((N - 1) * (N - 2))
 
-    return Hc
+    return hub_score
 
 
-def calculate_all_hub_scores(tprob):
+def calculate_all_hub_scores(msm):
     """
-    Calculate the hub scores for all states in a network defined by `tprob`.
+    Calculate the hub scores for all states in a network defined by `msm`.
 
     The "hub score" is a measure of how well traveled a certain state or
     set of states is in a network. Specifically, it is the fraction of
@@ -246,28 +161,19 @@ def calculate_all_hub_scores(tprob):
 
     Parameters
     ----------
-    tprob : matrix
-        The transition probability matrix
+    msm : mixtape.MarkovStateModel
+        MSM to analyze.
 
     Returns
     -------
-    Hc_array : nd_array, float
-        The hub score for each state in `tprob`
+    hub_scores : np.ndarray
+        The hub score for each state in `msm`
 
     See Also
     --------
-    calculate_fraction_visits : function
-        Calculate the fraction of times a state is visited on pathways going
-        from a set of "sources" to a set of "sinks".
-    calculate_hub_score : function
+    hubs.calculate_hub_score : function
         A function that computes just one hub score, can compute the hub score
         for a set of states.
-
-    Notes
-    -----
-    Employs dense linear algebra,
-      memory use scales as N^2
-      cycle use scales as N^6
 
     References
     ----------
@@ -275,26 +181,8 @@ def calculate_all_hub_scores(tprob):
         Article ASAP DOI: 10.1021/ct300537s
     """
 
-    N = tprob.shape[0]
-    states = list(range(N))
+    n_states = msm.n_states_
 
-    # calculate the hub score
-    Hc_array = np.zeros(N)
+    hub_scores = np.array([calculate_hub_score(msm, state) for state in xrange(n_states)])
 
-    # loop over each state and compute it's hub score
-    for i, waypoint in enumerate(states):
-
-        Hc = 0.0
-
-        # now loop over all combinations of sources/sinks and average
-        for s1 in states:
-            if waypoint != s1:
-                for s2 in states:
-                    if s1 != s2:
-                        if waypoint != s2:
-                            Hc += calculate_fraction_visits(tprob, waypoint, s1, s2)
-
-        # store the hub score in an array
-        Hc_array[i] = Hc / ((N - 1) * (N - 2))
-
-    return Hc_array
+    return hub_scores
