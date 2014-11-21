1c21b7b0beb3ddb8adeb74027e2b6be6118af646
Christian Schwantes <schwancr@stanford.edu>
2014-11-21 10:41:50 -0800

    added more comments as well as changed the citation to something relevant. I also renamed some variables to match the new reference

--- Mixtape/tpt/mfpt.py
+++ Mixtape/tpt/mfpt.py
@@ -18,6 +18,15 @@
 # License along with Mixtape. If not, see <http://www.gnu.org/licenses/>.
 """
 Functions for performing mean first passage time calculations for an MSM.
+
+For a useful introduction to Markov Chains (both ergodic and absorbing)
+check out Chapter 11 in:
+
+.. [1] Grinstead, C. M. and Snell, J. L. Introduction to Probability. 
+       American Mathematical Soc., 1998.
+
+The absorbing Markov Chain information is interesting, but note that
+we are using ergodic Markov Chains.
 """
 from __future__ import print_function, division, absolute_import
 import numpy as np
@@ -36,9 +45,15 @@ def mfpts(msm, sinks=None, lag_time=1.):
     ----------
     msm : mixtape.MarkovStateModel
         MSM fit to the data.
-    sinks : array, int
-        Indices of the sink states. If None, then all MFPTs will be
-        calculated using the Z-matrix formalism (which can be faster)
+    sinks : array_like, int, optional
+        Indices of the sink states. There are two use-cases:
+            - None [default] : All MFPTs will be calculated, and the
+                result is a matrix of the MFPT from state i to state j.
+                This uses the fundamental matrix formalism.
+            - list of ints or int : Only the MFPTs into these sink
+                states will be computed. The result is a vector, with
+                entry i corresponding to the average time it takes to 
+                first get to *any* sink state from state i
     lag_time : float, optional
         Lag time for the model. The MFPT will be reported in whatever
         units are given here. Default is (1) which is in units of the
@@ -47,60 +62,61 @@ def mfpts(msm, sinks=None, lag_time=1.):
     Returns
     -------
     mfpts : np.ndarray, float
-        MFPT in time units of lag_time, for each state (in order of state index)
+        MFPT in time units of lag_time, if sinks is None, then
+        mfpts[i, j] is the mean first passage time to state j
+        from state i. If sinks contains one or more states, then
+        mfpts[i] is the mean first passage time to one state in
+        sinks from state i.
 
     References
     ----------
-    .. [1] E, Weinan and Vanden-Eijnden, Eric Towards a Theory of Transition Paths
-           J. Stat. Phys. 123 503-523 (2006)
-    .. [2] Metzner, P., Schutte, C. & Vanden-Eijnden, E. Transition path theory
-           for Markov jump processes. Multiscale Model. Simul. 7, 1192-1219
-           (2009).
-    .. [3] Berezhkovskii, A., Hummer, G. & Szabo, A. Reactive flux and folding
-           pathways in network models of coarse-grained protein dynamics. J.
-           Chem. Phys. 130, 205102 (2009).
+    .. [1] Grinstead, C. M. and Snell, J. L. Introduction to 
+           Probability. American Mathematical Soc., 1998.    
+
+    As of November 2014, this chapter was available for free online:
+
+        http://www.dartmouth.edu/~chance/teaching_aids/books_articles/probability_book/Chapter11.pdf
     """
     populations = msm.populations_
     tprob = msm.transmat_
     n_states = msm.n_states_
 
     if sinks is None:
-
-        #eye = np.transpose(np.matrix(np.ones(num_states)))
-        # ^^^^!!!!!!!^^^^^ who wrote this and thought it was acceptable?!
-        # after plugging it into ipython, this just creates a column vector
-
-        #limiting_matrix = eye * populations
-        # since eye used to be a matrix, eye * populations was an
-        # outer product with the populations in each of the columns
-        # I'm pretty sure it's supposed to be in the rows, though...
+        # Use Thm 11.16 in [1]
         limiting_matrix = np.vstack([populations] * n_states)
-        #z = scipy.linalg.inv(scipy.sparse.eye(num_states, num_states) - (tprob - limiting_matrix))
-        z = scipy.linalg.inv(np.eye(n_states) - (tprob - limiting_matrix))
 
-        # mfpt[i,j] = z[j,j] - z[i,j] / pi[j]
-        mfpts = - z
-        for j in range(n_states):
-            mfpts[:, j] += z[j, j]
+        # Fundamental matrix
+        fund_matrix = scipy.linalg.inv(np.eye(n_states) - tprob + limiting_matrix)
+
+        # mfpt[i,j] = (fund_matrix[j,j] - fund_matrix[i,j]) / populations[j]
+        mfpts = fund_matrix * -1
+        for j in xrange(n_states):
+            mfpts[:, j] += fund_matrix[j, j]
             mfpts[:, j] /= populations[j]
 
         mfpts *= lag_time
 
     else:
+        # See section 11.5, and use Thm 11.5
+        # Turn our ergodic MSM into an absorbing one (all sink
+        # states are absorbing). Then calculate the mean time
+        # to absorption.
+        # Note: we are slightly modifying the description in
+        # 11.5 so that we also get the mfpts[sink] = 0.0
         sinks = np.array(sinks, dtype=int).reshape((-1,))
 
-        tprob = copy.copy(tprob)
+        absorb_tprob = copy.copy(tprob)
 
         for state in sinks:
-            tprob[state, :] = 0.0
-            tprob[state, state] = 2.0
+            absorb_tprob[state, :] = 0.0
+            absorb_tprob[state, state] = 2.0
 
-        tprob = tprob - np.eye(n_states)
+        lhs = np.eye(n_states) - absorb_tprob
 
-        rhs = -1 * np.ones(n_states)
+        rhs = np.ones(n_states)
         for state in sinks:
             rhs[state] = 0.0
 
-        mfpts = lag_time * np.linalg.solve(tprob, rhs)
+        mfpts = lag_time * np.linalg.solve(lhs, rhs)
 
     return mfpts
