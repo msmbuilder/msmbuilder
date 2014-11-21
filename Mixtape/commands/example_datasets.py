2ad872d9d2dbb84db42dcfab660f49880a7fcb29
Robert McGibbon <rmcgibbo@gmail.com>
2014-11-21 15:13:04 -0800

    longer description

--- Mixtape/commands/example_datasets.py
+++ Mixtape/commands/example_datasets.py
@@ -14,6 +14,7 @@ class DatasetCommand(NumpydocClassCommand):
 class AlanineDipeptideDatasetCommand(DatasetCommand):
     _concrete = True
     klass = AlanineDipeptide
+    description = 'Download example alanine dipeptide dataset.'
 
 
 class _NWellDatasetCommand(DatasetCommand):
@@ -27,18 +28,22 @@ class _NWellDatasetCommand(DatasetCommand):
 class DoubleWellDatasetCommand(_NWellDatasetCommand):
     _concrete = True
     klass = DoubleWell
+    description = 'Generate example double well potential dataset.'
 
 
 class QuadWellDatasetCommand(_NWellDatasetCommand):
     _concrete = True
     klass = QuadWell
+    description = 'Generate example quad-well potential dataset.'
 
 
 class FsPeptideDatasetCommand(DatasetCommand):
     _concrete = True
     klass = FsPeptide
+    description = 'Download example Fs-peptide dataset.'
 
 
 class MetEnkephalinDatasetCommand(DatasetCommand):
     _concrete = True
     klass = MetEnkephalin
+    description = 'Download example Met-Enkephalin dataset.'
