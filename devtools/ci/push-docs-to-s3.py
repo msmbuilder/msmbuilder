6c939e3fff34e1e4c40fc36535a2f9cb0c482c38
Robert T. McGibbon <rmcgibbo@gmail.com>
2014-12-01 17:07:00 -0800

    Update push-docs-to-s3.py

--- devtools/ci/push-docs-to-s3.py
+++ devtools/ci/push-docs-to-s3.py
@@ -7,7 +7,7 @@ import msmbuilder.version
 # on travis-ci to push the build documentation to Amazon S3.
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
-BUCKET_NAME = 'msmbuilder-mixtape'
+BUCKET_NAME = 'msmbuilder.org'
 
 bucket_name = AWS_ACCESS_KEY_ID.lower() + '-' + BUCKET_NAME
 conn = boto.connect_s3(AWS_ACCESS_KEY_ID,
