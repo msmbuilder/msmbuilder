df39149ea4c6cce737bc5fa6f1adc15463d9ccf7
Christian Schwantes <schwancr@stanford.edu>
2014-12-03 15:46:39 -0800

    remove themes. We now use msmb_themes.

--- doc/conf.py
+++ doc/conf.py
@@ -4,10 +4,7 @@ import sys
 import os
 import msmbuilder
 import msmbuilder.version
-
-sys.path.insert(0, os.path.abspath('themes/sphinx_rtd_theme-0.1.5'))
-import sphinx_rtd_theme
-
+import msmb_theme
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -107,16 +104,16 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "sphinx_rtd_theme"
+html_theme = "msmb_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {}
+html_theme_options = {'base_url' : 'http://msmbuilder.org'}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path = [msmb_theme.get_html_theme_path()]
 
 
 # The name for this set of Sphinx documents.  If None, it defaults to
