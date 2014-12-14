049436af65dff95a5343bbef652ed1a9ed3f148b
Robert McGibbon <rmcgibbo@gmail.com>
2014-12-13 16:43:12 -0800

    py3 travis

--- devtools/ci/install.sh
+++ devtools/ci/install.sh
@@ -12,6 +12,5 @@ export PATH=$HOME/miniconda/bin:$PATH
 
 conda update --yes conda
 conda config --add channels http://conda.binstar.org/omnia
-source activate $python
 conda install --yes conda-build
 
