bc65100a8abe88a91433ab0909803a21cd2a0426
Matthew Harrigan <harrigan@stanford.edu>
2015-03-13 17:25:34 -0700

    Remove mixtape compatibility aliases

--- msmbuilder/tica.py
+++
@@ -1,6 +0,0 @@
-import warnings
-
-warnings.warn("Please use the module decomposition instead of tica. " +
-              "This alias will be removed in version 3.1", DeprecationWarning)
-
-from .decomposition import *
