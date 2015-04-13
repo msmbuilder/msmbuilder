a0af4c326aec4e6a340d28490ecebb25b011ca97
kyleabeauchamp <kyleabeauchamp@gmail.com>
2015-04-13 19:44:14 -0400

    Update worksflow tests for featurizer --out and --transformed

--- msmbuilder/tests/workflows/test_1.sh
+++ msmbuilder/tests/workflows/test_1.sh
@@ -1,8 +1,8 @@
 msmb AlanineDipeptide
 msmb AtomIndices --out atom_indices.txt -p ~/msmbuilder_data/alanine_dipeptide/ala2.pdb \
      -d --heavy
-msmb AtomPairsFeaturizer --out atom_pairs --trjs '~/msmbuilder_data/alanine_dipeptide/*.dcd' \
-    --pair_indices atom_indices.txt --top ~/msmbuilder_data/alanine_dipeptide/ala2.pdb
+msmb AtomPairsFeaturizer --transformed atom_pairs --trjs '~/msmbuilder_data/alanine_dipeptide/*.dcd' \
+    --pair_indices atom_indices.txt --top ~/msmbuilder_data/alanine_dipeptide/ala2.pdb --out atom_pairs.pkl
 msmb tICA -i atom_pairs/ -t atom_pairs_tica.h5  --n_components 4 \
     --gamma 0 \
     --weighted_transform \
--- msmbuilder/tests/workflows/test_2.sh
+++ msmbuilder/tests/workflows/test_2.sh
@@ -2,10 +2,11 @@ msmb AlanineDipeptide
 msmb AtomIndices --out atom_indices.txt \
      -p ~/msmbuilder_data/alanine_dipeptide/ala2.pdb \
      -d --heavy
-msmb AtomPairsFeaturizer --out atom_pairs/ \
+msmb AtomPairsFeaturizer --transformed atom_pairs/ \
     --trjs '~/msmbuilder_data/alanine_dipeptide/*.dcd' \
     --pair_indices atom_indices.txt \
-    --top ~/msmbuilder_data/alanine_dipeptide/ala2.pdb
+    --top ~/msmbuilder_data/alanine_dipeptide/ala2.pdb \
+    --out atom_pairs.pkl
 msmb MiniBatchKMeans --n_clusters 100 \
     --batch_size 1000 \
     --inp atom_pairs \
