7cfc4bf2d5985a43e20a063f5b2572bb37c6c390
Robert McGibbon <rmcgibbo@gmail.com>
2015-05-08 14:06:39 -0700

    MAINT: Make exceptions in test_workflows more informative

--- msmbuilder/tests/test_workflows.py
+++ msmbuilder/tests/test_workflows.py
@@ -5,6 +5,33 @@ from pkg_resources import resource_filename
 from msmbuilder.tests.test_commands import tempdir
 
 
+class CalledProcessError(Exception):
+    def __init__(self, returncode, cmd, stdout=None, stderr=None):
+         self.returncode = returncode
+         self.cmd = cmd
+         self.stdout = stdout
+         self.stderr = stderr
+
+    def __str__(self):
+        return ("Command '%s' returned non-zero exit status %d.\n"
+                "\n==== stdout ====\n%s\n==== stderr ====\n%s\n") % (self.cmd, self.returncode,
+                                                 self.stdout, self.stderr)
+
+
+def check_call(cmd):
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    try:
+        stdout, stderr = process.communicate()
+    except:
+        process.kill()
+        process.wait()
+        raise
+
+    retcode = process.poll()
+    if retcode:
+        raise CalledProcessError(retcode, cmd, stdout=stdout, stderr=stderr)
+
+
 def shell_lines(resource):
     fn = resource_filename('msmbuilder', resource)
     buf = ''
@@ -23,16 +50,16 @@ def shell_lines(resource):
 def test_workflow_1():
     with tempdir():
         for line in shell_lines('tests/workflows/test_1.sh'):
-            subprocess.check_call(line.split())
+            check_call(line.split())
 
 
 def test_workflow_2():
     with tempdir():
         for line in shell_lines('tests/workflows/test_2.sh'):
-            subprocess.check_call(line.split())
+            check_call(line.split())
 
 
 def test_workflow_3():
     with tempdir():
         for line in shell_lines('tests/workflows/test_3.sh'):
-            subprocess.check_call(line.split())
+            check_call(line.split())
