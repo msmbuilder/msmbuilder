118ae6c77516c087400f4ac22b6bff33b334c4b5
Matthew Harrigan <harrigan@stanford.edu>
2015-08-12 16:13:19 -0700

    Update test for muller

--- msmbuilder/tests/test_muller.py
+++ msmbuilder/tests/test_muller.py
@@ -1,10 +1,18 @@
-from msmbuilder.example_datasets import MullerPotential, load_muller, load_doublewell
+from msmbuilder.example_datasets import load_muller, MullerPotential
+from msmbuilder.utils import array2d
 
 
-def test_1():
+def test_func():
     xx = load_muller(random_state=1)['trajectories']
     assert len(xx) == 10
     assert xx[0].ndim == 2
     assert xx[0].shape[1] == 2
+    array2d(xx)
 
-    print(load_muller())
+
+def test_class():
+    xx = MullerPotential(random_state=123122).get()['trajectories']
+    assert len(xx) == 10
+    assert xx[0].ndim == 2
+    assert xx[0].shape[1] == 2
+    array2d(xx)
