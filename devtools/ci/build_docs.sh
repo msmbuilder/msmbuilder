c3814cb6337b91d8b04d5653d832af42c08fba4f
Matthew Harrigan <harrigan@stanford.edu>
2015-10-12 16:57:06 -0700

    --install-local

--- devtools/ci/build_docs.sh
+++ devtools/ci/build_docs.sh
@@ -3,11 +3,14 @@
 # Print each line, exit on error
 set -ev
 
-conda install --yes `conda build devtools/conda-recipe --output`
+# Install the built package
+conda install --yes --use-local msmbuilder-dev
+
+# TODO: Make this a conda package, include in requirements.txt
 pip install msmb_theme
 
 # Install doc requirements
-conda install --yes `cat doc/requirements.txt | xargs`
+conda install --yes --file doc/requirements.txt
 
 cd doc
 make html
