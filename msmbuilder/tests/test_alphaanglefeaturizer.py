fd2ffbfd77a2b5a799835737be0b32e388ab2217
Brooke Husic <bhusic@stanford.edu>
2015-11-11 18:26:53 -0800

    test_alphaanglefeaturizer fix

--- msmbuilder/tests/test_alphaanglefeaturizer.py
+++ msmbuilder/tests/test_alphaanglefeaturizer.py
@@ -1,7 +1,6 @@
 import numpy as np
 from mdtraj.testing import eq, raises
 import msmbuilder.featurizer
-from msmbuilder.featurizer import subset_featurizer
 from msmbuilder.example_datasets import fetch_alanine_dipeptide
 
 def test_alanine_dipeptide():
@@ -16,11 +15,6 @@ def test_alanine_dipeptide():
 	assert(nothing[0].shape[1] == 0)
 
 from msmbuilder.example_datasets import fetch_fs_peptide
-#fs_peptide = FsPeptide()
-#import tempfile
-#import os
-#os.chdir(tempfile.mkdtemp())
-#from msmbuilder.dataset import dataset
 
 def test_fs_peptide():
 	# will produce 36 features
