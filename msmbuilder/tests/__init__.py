72fd1f58c49d6507e75d0a375c08e91a7815bae9
Matthew Harrigan <harrigan@stanford.edu>
2016-01-29 14:35:15 -0800

    test_alphaanglefeaturizer

--- msmbuilder/tests/__init__.py
+++ msmbuilder/tests/__init__.py
@@ -17,11 +17,21 @@ def my_warn(message, category=None, stacklevel=1):
     module = globals['__name__']
     filename = globals.get('__file__')
 
+    # Note: I went through each test (alphabetically) and added ignore rules.
+    # Each time I needed a new skip rule, I labeled the section with which
+    # file caused me to include it. Since these rules apply to all files,
+    # it would be incorrect to assume that each rule is *only* for the file
+    # in its section header.
+
+    # test_agglomerative
+
     if module == 'scipy._lib.decorator' and "inspect.getargspec() is deprecated" in message:
         return
 
     if module == 'mdtraj.formats.hdf5' and "inspect.getargspec() is deprecated" in message:
         return
+
+    # test_alphaanglefeaturizer
 
     return orig_warn(message=message, category=category,
                      stacklevel=stacklevel + 1)
