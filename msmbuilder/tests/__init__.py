ac95d0b53fc897ed154d37a4914f445a73b40009
Matthew Harrigan <harrigan@stanford.edu>
2016-02-16 12:09:58 -0800

    Put testing back to normal

--- msmbuilder/tests/__init__.py
+++ msmbuilder/tests/__init__.py
@@ -1,47 +1,9 @@
-import sys
 import warnings
-from warnings import warn as orig_warn
 
+# Show warnings for our package
+warnings.filterwarnings('always', module='msmbuilder.*')
 
-def my_warn(message, category=None, stacklevel=1):
-    # taken from warnings module
-    # Get context information
-    try:
-        caller = sys._getframe(stacklevel)
-    except ValueError:
-        globals = sys.__dict__
-        lineno = 1
-    else:
-        globals = caller.f_globals
-        lineno = caller.f_lineno
-    module = globals['__name__']
-    filename = globals.get('__file__')
-
-    m = {
-        'argspec': 'inspect.getargspec() is deprecated'
-    }
-
-    if module == 'scipy._lib.decorator' and m['argspec'] in message:
-        return
-
-    if module == 'mdtraj.formats.hdf5' and m['argspec'] in message:
-        return
-
-    if module == 'statsmodels.base.wrapper' and m['argspec'] in message:
-        return
-
-    if module == 'nose.util' and m['argspec'] in message:
-        return
-
-    print("Warning: module:  ", module)
-    print("Warning: message: ", message)
-
-    # This explicit check is necessary for python  < 3.5 maybe??
-    if category is None:
-        category = UserWarning
-
-    return orig_warn(message=message, category=category,
-                     stacklevel=stacklevel + 1)
-
-
-warnings.warn = my_warn
+# Show warnings for packages where we want to be conscious of warnings
+warnings.filterwarnings('always', module='mdtraj.*')
+warnings.filterwarnings('always', module='scipy.*')
+warnings.filterwarnings('always', module='sklearn.*')
