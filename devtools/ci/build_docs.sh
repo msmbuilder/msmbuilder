2690ed2dfbbb2217a8f3ed727391e85631fea7e6
Matthew Harrigan <harrigan@stanford.edu>
2016-02-19 18:18:08 -0800

    Use newer msmb_theme

--- devtools/ci/build_docs.sh
+++ devtools/ci/build_docs.sh
@@ -12,7 +12,7 @@ conda install --yes --use-local msmbuilder-dev
 # sphinx_rtd_theme's latest releases are not available
 # neither is msmb_theme
 # neither is sphinx > 1.3.1 (fix #1892 autodoc problem)
-pip install -I sphinx==1.3.5 sphinx_rtd_theme==0.1.9 msmb_theme==1.1.0
+pip install -I sphinx==1.3.5 sphinx_rtd_theme==0.1.9 msmb_theme==1.2.0
 
 # Install doc requirements
 conda install --yes --file doc/requirements.txt
