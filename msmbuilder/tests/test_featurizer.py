c106e4ef5dbe58fd092ad4aade1f82535e3ed656
Mohammad Muneeb Sultan <muneeb.sultan@gmail.com>
2016-03-22 15:42:47 -0700

    updated test

--- msmbuilder/tests/test_featurizer.py
+++ msmbuilder/tests/test_featurizer.py
@@ -87,18 +87,16 @@ def test_von_mises_featurizer():
                                               X_all[0].shape)
 
     #test to make sure results are being put in the right order
-    feat = VonMisesFeaturizer(["phi"], n_bins=10)
-    _, all_phi = md.compute_psi(trajectories[0])
+    feat = VonMisesFeaturizer(["phi","psi"], n_bins=10)
+    _, all_phi = md.compute_phi(trajectories[0])
     X_all = feat.transform(trajectories)
     all_res=[]
     for frame in all_phi:
         for dihedral_value in frame:
             all_res.extend(vm.pdf(dihedral_value,loc=feat.loc, kappa=feat.kappa))
-
     for k in range(10):
         rndint=np.random.randint(np.shape(X_all[0])[0])
-        X_all[0][rndint,:]==all_res[rndint*10:10+rndint*10]
-        assert (X_all == all_res).all()
+        assert(X_all[0][rndint,:10]==all_res[rndint*10:10+rndint*10]).all()
 
 
 
