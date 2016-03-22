8313b31a646b4eb1acafac497d95c2c4a4c28455
Matthew Harrigan <harrigan@stanford.edu>
2016-03-21 17:27:28 -0700

    Add clustering step

---
+++ msmbuilder/project_templates/4-cluster.py
@@ -0,0 +1,20 @@
+from msmbuilder.dataset2 import load_trajs, save_trajs, save_generic
+from msmbuilder.cluster import MiniBatchKMeans
+
+## Load
+meta, ttrajs = load_trajs('ttrajs')
+
+## Fit
+dim = 5
+kmeans = MiniBatchKMeans(n_clusters=500)
+kmeans.fit([traj[:, :dim] for traj in ttrajs.values()])
+
+## Transform
+ktrajs = {}
+for k, v in ttrajs.items():
+    ktrajs[k] = kmeans.partial_transform(v)
+
+## Save
+print(kmeans.summarize())
+save_trajs(ktrajs, 'ktrajs', meta)
+save_generic(kmeans, 'kmeans.pickl')
