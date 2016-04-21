c644f6022ba41904ab09789dd4cb72c8bab1d170
Matthew Harrigan <harrigan@stanford.edu>
2016-04-21 14:01:00 -0700

    expose render_meta

--- msmbuilder/io/__init__.py
+++ msmbuilder/io/__init__.py
@@ -1,4 +1,4 @@
 from .gather_metadata import gather_metadata, FAHParser, GenericParser
 from .io import (backup, preload_top, preload_tops, load_meta, load_generic,
-                 load_trajs, save_meta, save_generic, save_trajs,
+                 load_trajs, save_meta, render_meta, save_generic, save_trajs,
                  ProjectTemplate)
