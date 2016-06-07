64e19bb07970980554a0d46a4100ba41acd3058f
Carlos Hernandez <cxh@stanford.edu>
2016-06-07 14:26:02 -0700

    added new decomp methods

--- msmbuilder/decomposition/__init__.py
+++ msmbuilder/decomposition/__init__.py
@@ -4,3 +4,33 @@ from .pca import PCA, SparsePCA, MiniBatchSparsePCA
 from .sparsetica import SparseTICA
 from .tica import tICA
 from .ktica import KernelTICA
+from .base import MultiSequenceDecompositionMixin
+
+from sklearn import decomposition as _decomposition
+
+
+class FastICA(MultiSequenceDecompositionMixin, _decomposition.FastICA):
+    __doc__ = _decomposition.FastICA.__doc__
+
+    def summarize(self):
+        return '\n'.join([
+            "Independent Component Analysis (ICA)",
+            "----------",
+            "Number of components:    {n_components}",
+            "Number of iterations: {n_iter_}",
+        ]).format(**self.__dict__)
+
+
+class FactorAnalysis(MultiSequenceDecompositionMixin,
+                     _decomposition.FactorAnalysis):
+    __doc__ = _decomposition.FactorAnalysis.__doc__
+
+    def summarize(self):
+        return '\n'.join([
+            "FactorAnalysis (FA)",
+            "----------",
+            "Number of components:    {n_components}",
+            "Log likelihood:          {loglike_}",
+            "Noise variance:          {noise_variance_}",
+            "Number of iterations: {n_iter_}",
+        ]).format(**self.__dict__)
