68f8ebdcf9e5ef3823f300dfaa91131fe3497ad7
Matthew Harrigan <harrigan@stanford.edu>
2016-06-17 11:24:29 -0700

    More plots

--- msmbuilder/project_templates/analysis/gather-metadata-plot.py
+++ msmbuilder/project_templates/analysis/gather-metadata-plot.py
@@ -17,7 +17,7 @@ colors = sns.color_palette()
 meta = load_meta()
 
 
-## Plot logic
+## Plot histogram of trajectory lengths
 def plot_lengths(ax):
     lengths_ns = meta['nframes'] * (meta['step_ps'] / 1000)
     ax.hist(lengths_ns)
@@ -34,13 +34,49 @@ def plot_lengths(ax):
                 va='top',
                 )
 
+## Plot pie graph
+def plot_pie(ax):
+    lengths_ns = meta['nframes'] * (meta['step_ps'] / 1000)
+    sampling = lengths_ns.groupby(level=0).sum()
+
+    ax.pie(sampling,
+           shadow=True,
+           labels=sampling.index,
+           colors=sns.color_palette(),
+           )
+    ax.axis('equal')
+
+## Plot box plot
+def plot_boxplot(ax):
+    meta2 = meta.copy()
+    meta2['ns'] = meta['nframes'] * (meta['step_ps'] / 1000)
+    sns.boxplot(
+        x='proj', # TODO: make general
+        y='ns',
+        data=meta.reset_index(),
+        ax=ax,
+    )
+
+
 
 ## Plot
 fig, ax = plt.subplots(figsize=(7, 5))
 plot_lengths(ax)
 fig.tight_layout()
-fig.savefig("lengths.pdf")
-run(['xdg-open', 'lengths.pdf'])
+fig.savefig("lengths-hist.pdf")
+run(['xdg-open', 'lengths-hist.pdf'])
+
+fig, ax = plt.subplots(figsize=(7, 5))
+plot_pie(ax)
+fig.tight_layout()
+fig.savefig("lengths-pie.pdf")
+run(['xdg-open', 'lengths-pie.pdf'])
+
+fig, ax = plt.subplots(figsize=(7, 5))
+plot_boxplot(ax)
+fig.tight_layout()
+fig.savefig("lengths-boxplot.pdf")
+run(['xdg-open', 'lengths-boxplot.pdf'])
 
 ## Save metadata as html table
 render_meta(meta, 'meta.pandas.html')
