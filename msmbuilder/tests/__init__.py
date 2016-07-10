8bf3300f0aad36726687365714934496021da66a
Matthew Harrigan <harrigan@stanford.edu>
2016-07-09 19:13:16 -0700

    Only print warnings once

--- msmbuilder/tests/__init__.py
+++ msmbuilder/tests/__init__.py
@@ -5,5 +5,5 @@ warnings.filterwarnings('always', module='msmbuilder.*')
 
 # Show warnings for packages where we want to be conscious of warnings
 warnings.filterwarnings('always', module='mdtraj.*')
-warnings.filterwarnings('always', module='scipy.*')
-warnings.filterwarnings('always', module='sklearn.*')
+warnings.filterwarnings('default', module='scipy.*')
+warnings.filterwarnings('default', module='sklearn.*')
