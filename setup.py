472339ae5683724ec48129ec01c8665edf3685bb
Matthew Harrigan <harrigan@stanford.edu>
2016-10-18 12:24:43 -0700

    Release 3.6.1

--- setup.py
+++ setup.py
@@ -53,8 +53,8 @@ except ImportError:
     sys.exit(1)
 
 # #########################
-VERSION = '3.6.1.dev0'
-ISRELEASED = False
+VERSION = '3.6.1'
+ISRELEASED = True
 __version__ = VERSION
 # #########################
 
