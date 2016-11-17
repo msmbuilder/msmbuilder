95932d0b9167a0e534eb4cb1984e320b49545f3e
Jade Shi <jadeshi@vsp-compute-01.stanford.edu>
2016-11-16 16:10:00 -0800

    Updated test

--- msmbuilder/tests/test_mfpt_error.py
+++ msmbuilder/tests/test_mfpt_error.py
@@ -1,38 +1,28 @@
-import numpy as np
-import random
 from msmbuilder.tpt import mfpt
-from msmbuilder.msm import MarkovStateModel
-import matplotlib.pyplot as plt
+import numpy as np
 
-# Create fake 10x10 counts and tprob matrices
-ndim = 10
-sinks = [0] # Make state 0 the sink
-counts_range = range(100,200)
-counts = np.zeros((ndim, ndim))
-for i in range(ndim):
-    for j in range(ndim):
-        counts[i][j] = random.choice(counts_range)
-tprob = (counts.transpose() / np.sum(counts, axis=1)).transpose()
-# Insert into MSM template
-msm = MarkovStateModel()
-msm.transmat_ = tprob
-msm.countsmat_ = counts
-output = mfpt.mfpts(msm, sinks, errors=True, n_samples=1000)[:,1] # State 1 to 0 MFPT distribution with good statistics, approximately Gaussian/symmetric
 
-# Make state 1 out transitions poorly sampled, with either 0 or 1 "out counts" to every other state
-for i in range(10):
-    counts[1][i] = random.choice([1,0])
-counts[1][1] = random.choice(counts_range)
-tprob = (counts.transpose() / np.sum(counts, axis=1)).transpose()
-# Insert into MSM template
-msm.transmat_ = tprob
-msm.countsmat_ = counts
-bad_output = mfpt.mfpts(msm, sinks, errors=True, n_samples=1000)[:,1] # State 1 to 0 MFPT distribution with bad state 1 out-transition statistics, exponential-like with large outliers
+def test_create_perturb_params():
+    # Test with a 10x10 counts matrix, with all entries in the counts set to 100
+    countsmat = 100 * np.ones((10,10))
+    params = mfpt.create_perturb_params(countsmat)
+    # Check dimensions of outputs are equal to those of inputs
+    for param in params:
+        assert np.shape(param) == np.shape(countsmat) 
 
-# Plot results
-fig = plt.figure()
-plot = fig.add_subplot(111)
 
-plt.hist(output)
-plt.hist(bad_output)
-plt.show()
+def test_perturb_tmat():
+    # The transition matrix is perturbed under the CLT approximation, which is only valid for well-sampled data w.r.t. transition probability (tprob >> 1 / row-summed counts)
+    countsmat = 100 * np.ones((10,10)) # 10-state MSM, 1000 counts per state, 100 transition events between states, no zero entries
+    params = mfpt.create_perturb_params(countsmat)
+    new_transmat = (mfpt.perturb_tmat(params[0], params[1]))
+    # All transition probabilities are by design nonzero, so there should be no nonzero entries after the perturbation
+    assert len(np.where(new_transmat == 0)[0]) == 0
+    # Now let's assume you have a poorly sampled dataset where all elements in the counts matrix is 1
+    countsmat = np.ones((10,10))
+    params = mfpt.create_perturb_params(countsmat)
+    new_transmat = (mfpt.perturb_tmat(params[0], params[1]))
+    # Your perturbed transition matrix will have several negative values (set automatically to 0), indicating this method probably isn't appropriate for your dataset
+    # (This will also cause your distribution of MFPTs to have very obvious outliers to an otherwise approximately Gaussian distribution due to the artificial zeros in the transition matrix)
+    assert len(np.where(new_transmat == 0)[0] > 0)
+
