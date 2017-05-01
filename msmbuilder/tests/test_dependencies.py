06eba3b3b5f41e466de68411bd4d308f87bec24d
Brooke Husic <bhusic@stanford.edu>
2017-05-01 15:18:57 -0700

    use has_msmb_data function

--- msmbuilder/tests/test_dependencies.py
+++ msmbuilder/tests/test_dependencies.py
@@ -1,4 +1,5 @@
 import os, pip, sys, warnings
+from msmbuilder.example_datasets import has_msmb_data
 
 def test_installed_packages():
     installed_packages = pip.get_installed_distributions()
@@ -13,50 +14,6 @@ def test_installed_packages():
         if td not in package_names:
             raise RuntimeError('Please install {} to continue'.format(td))
 
-
-def test_fs_peptide():
-    try:
-        from msmbuilder.example_datasets import FsPeptide
-        fspeptide = FsPeptide().get()
-    except:
-        warnings.warn('Fs peptide dataset not found. Did you install msmb_data?')
-
-
-def test_alanine_dipeptide():
-    try:
-        from msmbuilder.example_datasets import AlanineDipeptide
-        adpeptide = AlanineDipeptide().get()
-    except:
-        warnings.warn('Alanine dipeptide dataset not found. Did you install msmb_data?')
-
-
-def test_double_well():
-    try:
-        from msmbuilder.example_datasets import DoubleWell
-        doublewell = DoubleWell().get()
-    except:
-        warnings.warn('Double well dataset not found. Did you install msmb_data?')
-
-
-def test_quad_well():
-    try:
-        from msmbuilder.example_datasets import QuadWell
-        quadwell = QuadWell().get()
-    except:
-        warnings.warn('Quad well dataset not found. Did you install msmb_data?')
-
-
-def test_metenkephalin():
-    try:
-        from msmbuilder.example_datasets import MetEnkephalin
-        metenkephalin = MetEnkephalin().get()
-    except:
-        warnings.warn('Met-enkephalin dataset not found. Did you install msmb_data?')
-
-
-def test_muller():
-    try:
-        from msmbuilder.example_datasets import MullerPotential
-        muller = MullerPotential().get()
-    except:
-        warnings.warn('Muller potential dataset not found. Did you install msmb_data?')
+def test_msmb_data():
+    if has_msmb_data() is None:
+        raise RuntimeError('Please install {} to continue'.format('msmb_data'))
