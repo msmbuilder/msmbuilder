57109bc8348b0ace736f7be6f3b387a34185e6fc
Brooke Husic <bhusic@stanford.edu>
2017-05-04 09:03:29 -0700

    remove old mean function

--- msmbuilder/utils/divergence.py
+++ msmbuilder/utils/divergence.py
@@ -14,18 +14,6 @@ def _sym_kld(P, Q, scalar=True):
     return _kld(P,Q,scalar=scalar) + _kld(Q,P,scalar=scalar)
 
 
-def _get_mean(P, Q):
-    if P.shape != Q.shape:
-        raise ValueError('P and Q must have the same shape')
-    M = []
-    for row in range(P.shape[0]):
-        temp = []
-        for i, entry in enumerate(P[row]):
-            temp.append((entry + Q[row][i])/2.)
-        M.append(temp)
-    return np.array(M)
-
-
 def _jsd(P, Q, scalar=True):
     M = np.mean([P, Q], axis=0)
     return 0.5 * _kld(P, M, scalar=scalar) + 0.5 * _kld(Q, M, scalar=scalar)
