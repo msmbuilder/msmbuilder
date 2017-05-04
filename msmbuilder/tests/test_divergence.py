9396f97c6873d3a1fbceb5038452ed8389a2e310
Brooke Husic <bhusic@stanford.edu>
2017-05-04 13:19:03 -0700

    changed all names and new test

--- msmbuilder/tests/test_divergence.py
+++ msmbuilder/tests/test_divergence.py
@@ -1,16 +1,17 @@
 import numpy as np
-from msmbuilder.utils.divergence import _kld
+from msmbuilder.cluster import LandmarkAgglomerative
+from msmbuilder.utils.divergence import *
 from scipy.stats import entropy
 
 def _get_random_prob_dist(n):
     P = np.random.random([n,n])
-    Q = np.random.random([n,n])
     P = (P.T / np.sum(P,axis=1)).T
-    Q = (Q.T / np.sum(Q,axis=1)).T
-    return P, Q
+    return P
+
 
 def test_kullback_leibler_manual():
-    P, Q = _get_random_prob_dist(4)
+    P = _get_random_prob_dist(4)
+    Q = _get_random_prob_dist(4)
 
     vec = []
     for row in range(P.shape[0]):
@@ -20,15 +21,103 @@ def test_kullback_leibler_manual():
         vec.append(temp)
     manual_kl = np.array(vec)
 
-    msmb_kl = _kld(P, Q, scalar=False)
+    msmb_kl = kl_divergence(P, Q, scalar=False)
 
     assert np.allclose(manual_kl, msmb_kl)
 
 
 def test_kullback_leibler_scipy():
-    P, Q = _get_random_prob_dist(4)
+    P = _get_random_prob_dist(4)
+    Q = _get_random_prob_dist(4)
 
     scipy_kl = entropy(P.T, Q.T)
-    msmb_kl = _kld(P, Q, scalar=False)  
+    msmb_kl = kl_divergence(P, Q, scalar=False)  
 
     assert np.allclose(scipy_kl, msmb_kl)
+
+
+def test_array_vs_msm():
+    my_list = [_get_random_prob_dist(4) for i in range(20)]
+
+    my_0 = np.array([x[0] for x in my_list])
+    my_1 = np.array([x[1] for x in my_list])    
+    my_2 = np.array([x[2] for x in my_list])    
+    my_3 = np.array([x[3] for x in my_list])    
+
+    my_flat = np.array([x.flatten() for x in my_list])
+
+    ind = np.random.randint(20)
+
+    dist0 = kl_divergence_array(my_0, my_0, ind)
+    dist1 = kl_divergence_array(my_1, my_1, ind)
+    dist2 = kl_divergence_array(my_2, my_2, ind)
+    dist3 = kl_divergence_array(my_3, my_3, ind)
+    dist_sum = dist0 + dist1 + dist2 + dist3
+
+    dist_all = kl_divergence_msm(my_flat, my_flat, ind)
+
+    assert np.allclose(dist_sum, dist_all)
+
+
+def test_array_vs_msm_sym():
+    my_list = [_get_random_prob_dist(4) for i in range(20)]
+
+    my_0 = np.array([x[0] for x in my_list])
+    my_1 = np.array([x[1] for x in my_list])    
+    my_2 = np.array([x[2] for x in my_list])    
+    my_3 = np.array([x[3] for x in my_list])    
+
+    my_flat = np.array([x.flatten() for x in my_list])
+
+    ind = np.random.randint(20)
+
+    dist0 = sym_kl_divergence_array(my_0, my_0, ind)
+    dist1 = sym_kl_divergence_array(my_1, my_1, ind)
+    dist2 = sym_kl_divergence_array(my_2, my_2, ind)
+    dist3 = sym_kl_divergence_array(my_3, my_3, ind)
+    dist_sum = dist0 + dist1 + dist2 + dist3
+
+    dist_all = sym_kl_divergence_msm(my_flat, my_flat, ind)
+
+    assert np.allclose(dist_sum, dist_all)
+
+
+def test_array_vs_msm_js():
+    my_list = [_get_random_prob_dist(4) for i in range(20)]
+
+    my_0 = np.array([x[0] for x in my_list])
+    my_1 = np.array([x[1] for x in my_list])    
+    my_2 = np.array([x[2] for x in my_list])    
+    my_3 = np.array([x[3] for x in my_list])    
+
+    my_flat = np.array([x.flatten() for x in my_list])
+
+    ind = np.random.randint(20)
+
+    dist0 = js_divergence_array(my_0, my_0, ind)
+    dist1 = js_divergence_array(my_1, my_1, ind)
+    dist2 = js_divergence_array(my_2, my_2, ind)
+    dist3 = js_divergence_array(my_3, my_3, ind)
+    dist_sum = dist0 + dist1 + dist2 + dist3
+
+    dist_all = js_divergence_msm(my_flat, my_flat, ind)
+
+    assert np.allclose(dist_sum, dist_all)
+
+
+def test_agglom_with_metric_array():
+    my_list = [_get_random_prob_dist(4) for i in range(20)]
+    my_stationary = np.array([x[0] for x in my_list])
+    model = LandmarkAgglomerative(n_clusters=2,
+                                  metric=sym_kl_divergence_array,
+                                  linkage='complete')
+    assert model.fit_predict([my_stationary])[0].shape == (20,)
+
+
+def test_agglom_with_metric_msm():
+    my_list = [_get_random_prob_dist(4) for i in range(20)]
+    my_flat = np.array([x.flatten() for x in my_list])
+    model = LandmarkAgglomerative(n_clusters=2,
+                                  metric=sym_kl_divergence_msm,
+                                  linkage='complete')
+    assert model.fit_predict([my_flat])[0].shape == (20,)
--- msmbuilder/utils/divergence.py
+++ msmbuilder/utils/divergence.py
@@ -2,7 +2,7 @@ from __future__ import print_function, division, absolute_import
 import numpy as np
 from scipy.stats import entropy
 
-def _kld(P, Q, scalar=True):
+def kl_divergence(P, Q, scalar=True):
     result = entropy(P.T, Q.T)
     if scalar:
         return np.sum(result)
@@ -10,25 +10,26 @@ def _kld(P, Q, scalar=True):
         return result
 
 
-def _sym_kld(P, Q, scalar=True):
-    return _kld(P,Q,scalar=scalar) + _kld(Q,P,scalar=scalar)
+def sym_kl_divergence(P, Q, scalar=True):
+    return kl_divergence(P,Q,scalar=scalar) + kl_divergence(Q,P,scalar=scalar)
 
 
-def _jsd(P, Q, scalar=True):
+def js_divergence(P, Q, scalar=True):
     M = np.mean([P, Q], axis=0)
-    return 0.5 * _kld(P, M, scalar=scalar) + 0.5 * _kld(Q, M, scalar=scalar)
+    return (0.5 * kl_divergence(P, M, scalar=scalar) +
+            0.5 * kl_divergence(Q, M, scalar=scalar))
 
 
-def kl_divergence(target, ref, i):
-    return np.array([_kld(ref[i],t) for t in target])
+def kl_divergence_array(target, ref, i):
+    return np.array([kl_divergence(ref[i],t) for t in target])
 
 
-def symmetric_kl_divergence(target, ref, i):
-    return np.array([_sym_kld(ref[i],t) for t in target])
+def sym_kl_divergence_array(target, ref, i):
+    return np.array([sym_kl_divergence(ref[i],t) for t in target])
 
 
-def js_divergence(target, ref, i):
-    return np.array([_jsd(ref[i],t) for t in target])
+def js_divergence_array(target, ref, i):
+    return np.array([js_divergence(ref[i],t) for t in target])
 
 
 def _make_square(sequence):
@@ -37,12 +38,12 @@ def _make_square(sequence):
 
 
 def kl_divergence_msm(target, ref, i):
-    return kl_divergence(_make_square(target), _make_square(ref), i)
+    return kl_divergence_array(_make_square(target), _make_square(ref), i)
 
 
-def symmetric_kl_divergence_msm(target, ref, i):
-    return symmetric_kl_divergence(_make_square(target), _make_square(ref), i)
+def sym_kl_divergence_msm(target, ref, i):
+    return sym_kl_divergence_array(_make_square(target), _make_square(ref), i)
 
 
 def js_divergence_msm(target, ref, i):
-    return js_divergence(_make_square(target), _make_square(ref), i)
+    return js_divergence_array(_make_square(target), _make_square(ref), i)
