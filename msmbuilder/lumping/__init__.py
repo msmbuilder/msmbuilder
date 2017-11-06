64cf7a3130115ad515fda242f8632ff87112a499
Brooke Husic <bhusic@stanford.edu>
2017-11-05 16:59:30 -0800

    bace

--- msmbuilder/lumping/__init__.py
+++ msmbuilder/lumping/__init__.py
@@ -3,5 +3,6 @@ from __future__ import absolute_import, print_function, division
 from .pcca import PCCA
 from .pcca_plus import PCCAPlus
 from .mvca import MVCA
+from .bace import BACE
 
-__all__ = ["PCCA", "PCCAPlus", "MVCA"]
+__all__ = ["PCCA", "PCCAPlus", "MVCA", "BACE"]
